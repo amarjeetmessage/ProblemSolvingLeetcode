a531c23b8f4d062b46646c6419b8256ab43273c8
Amarjeet <amarjeet.btechcse@gmail.com>
2025-09-07 09:38:07 +0530

    Time: 0 ms (100%), Space: 42.2 MB (5.02%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.java
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.java
@@ -8,7 +8,7 @@ class Solution {
             }
         }
         else {
-            for(int i = 0; i< n-1; i= i+2) {
+            for(int i = 0; i< n-2; i= i+2) {
                 arr[i] = i+1;
                 arr[i+1] = -(i+1);
             }
