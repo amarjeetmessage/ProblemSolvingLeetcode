e6e50e128325b4727b7638ee98a930b434820006
Amarjeet <amarjeet.btechcse@gmail.com>
2025-10-03 11:13:20 +0530

    Time: 0 ms (100%), Space: 44.6 MB (50.15%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,81 +11,20 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
+        if(head == null){
+            return false;
+        }
+        
         ListNode slow = head;
         ListNode fast = head.next;
 
         while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
-            if(slow == fast){
-                return true;
+            if(slow == fast ){
+                return true;//cycle detected
             }
         }
-        return false;
-
-    }
-}
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
-public class Solution {
-    public boolean hasCycle(ListNode head) {
-
-        if(head == null) return false;
-        ListNode slow = head , fast = head.next;
-
-        while(fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
-            if(slow == fast ) {
-                return true;
-            }
-        }
-        return false;
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // if (head == null) {
-        //     return false;
-        // }
-
-        // ListNode slow = head, fast = head;
-
-        // while (fast != null && fast.next != null) {
-        //     slow = slow.next;
-        //     fast = fast.next.next;
-
-        //     if (slow == fast) { // Cycle detected
-        //         return true;
-        //     }
-        // }
-        // return false; // No cycle
+        return false;//cycle does not exist 
     }
 }
