7962c978ae8d7de7824e4b0dbf588a258d3af2e7
Amarjeet <amarjeet.btechcse@gmail.com>
2025-10-09 19:03:46 +0530

    Time: 0 ms (100%), Space: 41.5 MB (23.49%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -10,13 +10,17 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
+        // Two pointer approach: slow and fast
         ListNode slow = head;
         ListNode fast = head;
 
-        while(fast != null && fast.next != null){
+        // Move fast by 2 and slow by 1
+        while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
+
+        // When fast reaches end, slow will be at middle
         return slow;
     }
 }
