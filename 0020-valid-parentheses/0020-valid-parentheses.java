aee9d1f0ad8f40fb01c3cca03268ff2d7ee9d4ed
Amarjeet <amarjeet.btechcse@gmail.com>
2025-10-18 18:06:41 +0530

    Time: 2 ms (97.36%), Space: 42.1 MB (24.17%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,46 @@
+// import java.util.Stack;
+
+// class Solution {
+//     public boolean isValid(String s) {
+//         Stack<Character> st = new Stack<>();
+
+//         for (char c : s.toCharArray()) {
+//             if (c == '(' || c == '{' || c == '[') {
+//                 st.push(c);
+//             } else {
+//                 if (st.isEmpty()) return false;
+
+//                 char top = st.pop();
+//                 if ((c == ')' && top != '(') ||
+//                     (c == '}' && top != '{') ||
+//                     (c == ']' && top != '[')) {
+//                     return false;
+//                 }
+//             }
+//         }
+
+//         return st.isEmpty();
+//     }
+// }
+
+
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character> st = new Stack<>();
+
+        for(char c: s.toCharArray()){
+            if(c == '('){
+                st.push(')');
+            }else if(c == '{'){
+                st.push('}');
+            }else if(c == '['){
+                st.push(']');
+            }else if(st.isEmpty() || st.pop() != c){
+                return false;
+            }
+
+        }
+            return st.isEmpty();
+    }
+}
