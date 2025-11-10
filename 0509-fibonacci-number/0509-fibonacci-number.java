b75a4a33de3b6bbaafe3dcd5bf24b1743961c736
Amarjeet <amarjeet.btechcse@gmail.com>
2025-11-10 17:41:58 +0530

    Time: 0 ms (100%), Space: 42 MB (8.83%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,8 +1,30 @@
+// class Solution {
+//     public int fib(int n) {
+//         if(n == 0 || n==1){
+//             return n;
+//         }
+//         return fib(n-1) + fib(n-2);
+//     }
+// }
+
 class Solution {
-    public int fib(int n) {
-        if(n == 0 || n==1){
-            return n;
+
+    public static int findFibb(int n, int[] dp) {
+        //base case 
+        if(n == 0 || n == 1) return n;
+        
+        //check first if it exists
+        if(dp[n] != -1) {
+            return dp[n];
         }
-        return fib(n-1) + fib(n-2);
+
+        dp[n] = findFibb(n-1, dp) + findFibb(n-2, dp);
+        return dp[n];
+    }
+    public int fib(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+
+        return findFibb(n, dp);
     }
 }
