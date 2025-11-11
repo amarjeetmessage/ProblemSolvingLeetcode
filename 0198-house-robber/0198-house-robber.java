b4e161dd530bd88cb08fd9ea3907a34f2ad23e31
Amarjeet <amarjeet.btechcse@gmail.com>
2025-11-11 18:20:50 +0530

    Time: 0 ms (100%), Space: 43 MB (7.7%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,18 +1,25 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if (n == 0) return 0;
-        if (n == 1) return nums[0];
-        if (n == 2) return Math.max(nums[0], nums[1]);
-
         int[] dp = new int[n];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-
-        for (int i = 2; i < n; i++) {
-            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        Arrays.fill(dp , -1);
+        return profit(nums, n-1, dp);
+    }
+    public static int profit(int nums[], int n, int[] dp) {
+        //base case
+        if(n < 0) return 0; //or if(n == 1) return nums[1];
+        if(n == 0) return nums[0];
+        //check it exist
+        if(dp[n] != -1) {
+            return dp[n];
         }
 
-        return dp[n - 1];
+        //recurrence relation 
+        int pick = profit(nums ,n-2, dp) + nums[n];
+        int notPick = profit(nums, n-1, dp);
+
+        dp[n] = Math.max(pick , notPick);
+        return dp[n];
+        
     }
 }
