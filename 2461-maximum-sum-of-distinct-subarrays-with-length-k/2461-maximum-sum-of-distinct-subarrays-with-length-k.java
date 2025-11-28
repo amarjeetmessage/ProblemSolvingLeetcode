02a02af95a32f88f7f8e9d433aca9ee59a6ffd67
Amarjeet <amarjeet.btechcse@gmail.com>
2025-11-28 07:03:45 +0530

    Time: 58 ms (20.02%), Space: 99.5 MB (5.9%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,26 +1,59 @@
+// class Solution {
+//     public long maximumSubarraySum(int[] nums, int k) {
+//         long max = 0;
+//         for(int i = 0; i<=nums.length - k; i++) {
+//             Set<Integer> set = new HashSet<>();
+//             boolean isDup = false;
+//             long sum = 0;
+
+
+//             for(int j = i; j<i+k; j++) {
+//                 if(set.contains(nums[j])) {
+//                     isDup = true;
+//                     break;
+//                 }
+//                 set.add(nums[j]);
+
+//                 sum = sum + nums[j];
+//             }
+//             if(!isDup) {
+//                 max = Math.max(max, sum);
+//             }
+            
+//         }
+//         return max;
+//     }
+// }
+
+
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        long sum = 0;
         long max = 0;
-        for(int i = 0; i<=nums.length - k; i++) {
-            Set<Integer> set = new HashSet<>();
-            boolean isDup = false;
-            long sum = 0;
+        int left = 0;
 
+        for(int right = 0; right < nums.length; right++) {
+            // Expand window
+            sum += nums[right];
+            freq.put(nums[right], freq.getOrDefault(nums[right], 0) + 1);
 
-            for(int j = i; j<i+k; j++) {
-                if(set.contains(nums[j])) {
-                    isDup = true;
-                    break;
+            // If size > k, shrink from left
+            if(right - left + 1 > k) {
+                freq.put(nums[left], freq.get(nums[left]) - 1);
+                if(freq.get(nums[left]) == 0) {
+                    freq.remove(nums[left]);
                 }
-                set.add(nums[j]);
-
-                sum = sum + nums[j];
+                sum -= nums[left];
+                left++;
             }
-            if(!isDup) {
+
+            // Check if window is valid (size k & all unique)
+            if(right - left + 1 == k && freq.size() == k) {
                 max = Math.max(max, sum);
             }
-            
         }
+
         return max;
     }
 }
