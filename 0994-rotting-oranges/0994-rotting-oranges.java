eee58c35ba39b3ccb270e383b2f0d9214edbb74f
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-01-17 12:00:24 +0530

    Time: 2 ms (90.74%), Space: 44.1 MB (62.08%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,65 +1,50 @@
 class Solution {
-    int rows, cols;
-    int[][] time;
-    int[][] dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-
     public int orangesRotting(int[][] grid) {
-        rows = grid.length;
-        cols = grid[0].length;
-        time = new int[rows][cols];
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int fresh = 0;
+        Queue<int[]> q = new LinkedList<>();
 
-        // Step 1: initialize time matrix
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (grid[i][j] == 2) {
-                    time[i][j] = 0;          // rotten -> time 0
-                } else {
-                    time[i][j] = Integer.MAX_VALUE; // fresh or empty -> INF
+                    q.offer(new int[]{i, j});
+                } else if (grid[i][j] == 1) {
+                    fresh++;
                 }
             }
         }
 
-        // Step 2: run DFS from every rotten orange
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (grid[i][j] == 2) {
-                    dfs(i, j, 0, grid);
+        if (fresh == 0) return 0;
+
+        int minutes = 0;
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            boolean rotted = false;
+
+            for (int i = 0; i < size; i++) {
+                int[] cell = q.poll();
+                int r = cell[0], c = cell[1];
+
+                for (int[] d : dirs) {
+                    int nr = r + d[0];
+                    int nc = c + d[1];
+
+                    if (nr >= 0 && nr < rows && nc >= 0 && nc < cols 
+                        && grid[nr][nc] == 1) {
+                        grid[nr][nc] = 2;
+                        fresh--;
+                        q.offer(new int[]{nr, nc});
+                        rotted = true;
+                    }
                 }
             }
+
+            if (rotted) minutes++;
         }
 
-        // Step 3: find answer
-        int ans = 0;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-
-                if (grid[i][j] == 1 && time[i][j] == Integer.MAX_VALUE) {
-                    return -1;   // unreachable fresh orange
-                }
-
-                if (grid[i][j] != 0) {
-                    ans = Math.max(ans, time[i][j]);
-                }
-            }
-        }
-        return ans;
-    }
-
-    private void dfs(int r, int c, int t, int[][] grid) {
-
-        // explore 4 directions
-        for (int[] d : dirs) {
-            int nr = r + d[0];
-            int nc = c + d[1];
-
-            if (nr >= 0 && nr < rows && nc >= 0 && nc < cols) {
-
-                // only rot fresh oranges
-                if (grid[nr][nc] == 1 && t + 1 < time[nr][nc]) {
-                    time[nr][nc] = t + 1;
-                    dfs(nr, nc, t + 1, grid);
-                }
-            }
-        }
+        return fresh == 0 ? minutes : -1;
     }
 }
