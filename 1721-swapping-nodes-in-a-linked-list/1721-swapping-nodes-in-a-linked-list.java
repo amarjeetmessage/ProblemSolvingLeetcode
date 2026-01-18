64ec7d2dd9a26634c2f57dea8312ff8e96235c90
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-01-18 14:22:32 +0530

    Time: 1 ms (100%), Space: 119.4 MB (19.54%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -8,27 +8,69 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+
+ ////approach 1st
+// class Solution {
+//     public ListNode swapNodes(ListNode head, int k) {
+
+//         ListNode curr = head;
+//         int len = 0;
+//         while(curr != null) {
+//             len = len + 1;
+//             curr = curr.next;
+//         }
+//         ListNode n1 = head, n2 = head;
+//         int jump1 = k -1;
+//         int jumpcount1 = 0;
+//         while(jumpcount1 < jump1) {
+//             jumpcount1++;
+//             n1 = n1.next;
+//         }
+
+//         int jump2 = len - k;
+//         int jumpcount2 = 0;
+//         while(jumpcount2 < jump2) {
+//             jumpcount2++;
+//             n2 = n2.next;
+//         }
+
+//         int temp = n1.val;
+//         n1.val = n2.val;
+//         n2.val = temp;
+
+//         return head; 
+//     }
+// }
+
+// //TC = O(N)
+// //SC = O(1)
+
+
+
+
+
+
+
+
+
+
+
+
+//approach 2nd 
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-
-        ListNode curr = head;
-        int len = 0;
-        while(curr != null) {
-            len = len + 1;
-            curr = curr.next;
-        }
-        ListNode n1 = head, n2 = head;
-        int jump1 = k -1;
-        int jumpcount1 = 0;
-        while(jumpcount1 < jump1) {
-            jumpcount1++;
+        ListNode n1 = head;
+        int startJump = 0;
+        while(startJump < k -1 ) {
+            startJump++;
             n1 = n1.next;
-        }
 
-        int jump2 = len - k;
-        int jumpcount2 = 0;
-        while(jumpcount2 < jump2) {
-            jumpcount2++;
+        }
+        ListNode nT1 = n1;
+        ListNode n2 = head;
+        while(nT1.next != null) {
+            nT1 = nT1.next;
             n2 = n2.next;
         }
 
@@ -36,6 +78,7 @@ class Solution {
         n1.val = n2.val;
         n2.val = temp;
 
-        return head; 
+        return head;
+
     }
 }
