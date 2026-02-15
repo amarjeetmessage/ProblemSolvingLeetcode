54d5f4c482137c03d918870a206e7f209b4a90b1
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-02-15 11:12:41 +0530

    Time: 52 ms (5.28%), Space: 136.3 MB (14.98%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
@@ -7,7 +7,7 @@ class Solution {
             int v1 = edges[i][0];
             int v2 = edges[i][1];
 
-            freqMap.put(v1,freqMap.getOrDefault(v1, 0) + 1);
+            freqMap.put(v1,freqMap.getOrDefault(v1, 0) + 1);// key == v1, value == freqMap.getOrDefault(v1, 0)
             freqMap.put(v2,freqMap.getOrDefault(v2, 0) + 1);
 
             if(freqMap.get(v1) == len) {
