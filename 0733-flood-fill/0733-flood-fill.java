49cb619cdf98b582c7b0e3b6f156e0b6014ddc95
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-02-15 19:54:12 +0530

    Time: 1 ms (45.84%), Space: 46.8 MB (62.84%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,26 +1,28 @@
 class Solution {
-    int rows, cols;
-    int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-    
+    int rows;
+    int cols;
+
+    public void dfs(int row, int col, int newColor, int currColor, boolean visited[][], int image[][]) {
+        if(row<0 || row>=rows || col<0 || col>=cols || image[row][col] != currColor || visited[row][col]) {
+            return;
+        }
+
+        image[row][col] = newColor;
+        visited[row][col] = true;
+
+        //visit neighbours [up, right, down, left]
+        int adjList[][] = {{row-1,col},{row+1,col},{row,col+1},{row,col-1}};
+        for(int neighbour[] : adjList) {
+            dfs(neighbour[0], neighbour[1], newColor, currColor, visited,image);
+        }
+    }
+
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         rows = image.length;
         cols = image[0].length;
-        int oldColor = image[sr][sc];
-        if (oldColor != color) {
-            dfs(image, sr, sc, oldColor, color);
-        }
+        boolean[][] visited = new boolean[rows][cols];
+        dfs(sr,sc,color,image[sr][sc],visited,image);
         return image;
-    }
-
-    private void dfs(int[][] image, int r, int c, int oldColor, int newColor) {
-        image[r][c] = newColor;
-        for (int[] d : dirs) {
-            int nr = r + d[0];
-            int nc = c + d[1];
-            if (nr >= 0 && nr < rows && nc >= 0 && nc < cols 
-                && image[nr][nc] == oldColor) {
-                dfs(image, nr, nc, oldColor, newColor);
-            }
-        }
+        
     }
 }
