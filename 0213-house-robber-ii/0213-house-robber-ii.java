4b4c7af0b545e1bc3b10e9114274170586b7fc07
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-02-19 15:33:41 +0530

    Time: 0 ms (100%), Space: 43.1 MB (21.36%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,33 +1,35 @@
 class Solution {
+    public int rob1(int[] nums) {
+        int size = nums.length;
+        if(size == 0) return 0;
+        if(size == 1) return nums[0];
+        int prev2 = 0;
+        int prev1 = nums[0];
+
+        for(int i = 2;i<=size;i++){
+            int pick = nums[i-1] + prev2;
+            int noPick = prev1;
+            int ans = Math.max(pick,noPick);
+            prev2 = prev1;
+            prev1 = ans;
+        }
+        return prev1;
+    }
     public int rob(int[] nums) {
         int n = nums.length;
-
+        if(n == 0) return 0;
         if(n == 1) return nums[0];
-        if(n == 2) return Math.max(nums[0] , nums[1]);
-
-        int[] dp = new int[n];
-        Arrays.fill(dp , -1);
-        int a = profit(nums, n-1, dp, 1);
-        int[] ap = new int[n];
-        Arrays.fill(ap , -1);
-        int b = profit(nums, n-2, ap, 0);
-        return Math.max(a,b);
-    }
-    public static int profit(int nums[], int n, int[] dp,int i) {
-        //base case
-        if(n < i) return 0; //or if(n == 1) return nums[1];
-        
-        //check it exist
-        if(dp[n] != -1) {
-            return dp[n];
+        int[] num1 = new int[n-1];
+        int[] num2 = new int[n-1];
+        int j = 0, k = 0;
+        for(int i = 0;i<n;i++){
+            if(i != 0){
+                num1[j++] = nums[i];
+            }
+            if(i != n-1){
+                num2[k++] = nums[i];
+            }
         }
-
-        //recurrence relation 
-        int pick = profit(nums ,n-2, dp,i) + nums[n];
-        int notPick = profit(nums, n-1, dp,i);
-
-        dp[n] = Math.max(pick , notPick);
-        return dp[n];
-        
+        return Math.max(rob1(num1),rob1(num2));
     }
 }
