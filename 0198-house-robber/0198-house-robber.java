0188156e2806da3cb3dc140e76079042f57f981d
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-03-09 16:39:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,24 +1,46 @@
 class Solution {
     public int rob(int[] nums) {
+        //dp always 1 based indexing
         int n = nums.length;
-
         int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
 
-        return solve(nums, n-1,dp);
-    }
-    public static int solve(int[] nums, int n, int[] dp) {
-        if(n == 0 ) return nums[n];
-        if(n < 0) return 0;
+        dp[1] = nums[0];
+        dp[2] = Math.max(nums[0], nums[1]);
 
-        if(dp[n] != -1) {
-            return dp[n];
+        for(int i = 3; i<=n; i++) {
+            // dp[] = dp[i-1];
+            // dp[] = nums[i] + dp[i-2];
+
+            dp[i] = Math.max(dp[i-1], nums[i] + dp[i-2]);
         }
-
-        int a = solve(nums, n-1,dp);
-        int b = nums[n] + solve(nums, n-2,dp);
-
-        dp[n] = Math.max(a,b);
         return dp[n];
     }
 }
+
+ 
+
+// class Solution {
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+
+//         int[] dp = new int[n+1];
+//         Arrays.fill(dp, -1);
+
+//         return solve(nums, n-1,dp);
+//     }
+//     public static int solve(int[] nums, int n, int[] dp) {
+//         if(n == 0 ) return nums[n];
+//         if(n < 0) return 0;
+
+//         if(dp[n] != -1) {
+//             return dp[n];
+//         }
+
+//         int a = solve(nums, n-1,dp);
+//         int b = nums[n] + solve(nums, n-2,dp);
+
+//         dp[n] = Math.max(a,b);
+//         return dp[n];
+//     }
+// }
+
