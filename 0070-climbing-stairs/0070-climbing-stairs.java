f6808c21868744e8400d864e182771e1d5d796f8
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-03-09 16:00:06 +0530

    Time: 0 ms (100%), Space: 41.8 MB (96.76%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -4,7 +4,10 @@ class Solution {
 
         // if(n<=2) return n;
         dp[1] = 1;
-        dp[2] = 2;
+        if(n >= 2) {
+            dp[2] = 2;
+        }
+        
 
         for(int i = 3; i<= n; i++) {
             dp[i] = dp[i-1] + dp[i-2];
