086ff135f9abca560e7fdd15e3f6db6fb4258308
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-03-09 07:38:45 +0530

    Time: 14 ms (14.52%), Space: 46.9 MB (81.61%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,16 +1,40 @@
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         for(int i = 0; i<nums.length; i++) {
+//             int count = 0;
+//             for(int j = 0; j<nums.length; j++) {
+//                 if(nums[i] == nums[j]) {
+//                     count++;
+//                 }
+//             }
+//             if(count == 1) {
+//                 return nums[i];
+//             }
+//         }
+//         return -1;
+//     }
+// }
 class Solution {
     public int singleNumber(int[] nums) {
-        for(int i = 0; i<nums.length; i++) {
-            int count = 0;
-            for(int j = 0; j<nums.length; j++) {
-                if(nums[i] == nums[j]) {
-                    count++;
-                }
-            }
-            if(count == 1) {
-                return nums[i];
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Step 1: Count frequency of each number
+        for(int num : nums) {
+            if(map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
             }
         }
+
+        // Step 2: Find number with frequency = 1
+        for(int num : map.keySet()) {
+            if(map.get(num) == 1) {
+                return num;
+            }
+        }
+
         return -1;
     }
 }
