086fe6165b964b7eb4e2bcedbccbd750b496153a
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-03-11 15:48:01 +0530

    Time: 0 ms (100%), Space: 43.8 MB (23.3%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,33 +1,122 @@
+// class Solution {
+//     public int uniquePathsWithObstacles(int[][] grid) {
+
+//         int m = grid.length;
+//         int n = grid[0].length;
+
+//         int[][] dp = new int[m+1][n+1];
+
+//         for(int[] row : dp) {
+//             Arrays.fill(row, -1);
+//         }
+
+//         return solve(0,0,m,n,grid,dp);
+//     }
+
+//     public int solve(int i, int j, int m, int n, int[][] grid,int[][] dp){
+
+//         if(i >= m || j >= n) return 0;
+
+//         if(grid[i][j] == 1) return 0;//this line is extra as compared to unique path 1;
+
+//         if(i == m-1 && j == n-1) return 1;
+
+//         if(dp[i][j] != -1) {
+//             return dp[i][j];
+//         }
+
+//         int right = solve(i, j+1, m, n, grid,dp);
+//         int down = solve(i+1, j, m, n, grid,dp);
+
+//         return dp[i][j] = right + down;
+//     }
+// }
+
+
+
+
+
+
+
+// class Solution {
+//     public int uniquePathsWithObstacles(int[][] grid) {
+
+//         int m = grid.length;
+//         int n = grid[0].length;
+
+//         int[][] dp = new int[m][n];
+
+//         // start cell
+//         if(grid[0][0] == 1) return 0;
+//         dp[0][0] = 1;
+
+//         for(int i = 0; i < m; i++){
+//             for(int j = 0; j < n; j++){
+
+//                 if(grid[i][j] == 1){
+//                     dp[i][j] = 0;
+//                     continue;
+//                 }
+
+//                 if(i > 0)
+//                     dp[i][j] += dp[i-1][j];
+
+//                 if(j > 0)
+//                     dp[i][j] += dp[i][j-1];
+//             }
+//         }
+
+//         return dp[m-1][n-1];
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
-    public int uniquePathsWithObstacles(int[][] grid) {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
 
-        int m = grid.length;
-        int n = grid[0].length;
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
 
-        int[][] dp = new int[m+1][n+1];
+        long[][] dp = new long[m + 1][n + 1];
 
-        for(int[] row : dp) {
-            Arrays.fill(row, -1);
+        if (obstacleGrid[m - 1][n - 1] == 1) {
+            return 0;//b/c not possible to reach at destination
         }
 
-        return solve(0,0,m,n,grid,dp);
-    }
+        // for(int i=0;i<=m;i++){
+        //     dp[i][n]=0;
+        // }
+        // for(int j=0;j<=n;j++){
+        //     dp[m][j]=0;
+        // }
 
-    public int solve(int i, int j, int m, int n, int[][] grid,int[][] dp){
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
 
-        if(i >= m || j >= n) return 0;
+                if (i == m - 1 && j == n - 1) {
+                    dp[i][j] = 1;
+                    continue;
+                }
 
-        if(grid[i][j] == 1) return 0;//this line is extra as compared to unique path 1;
-
-        if(i == m-1 && j == n-1) return 1;
-
-        if(dp[i][j] != -1) {
-            return dp[i][j];
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+                }
+            }
         }
 
-        int right = solve(i, j+1, m, n, grid,dp);
-        int down = solve(i+1, j, m, n, grid,dp);
-
-        return dp[i][j] = right + down;
+        return (int) dp[0][0];
     }
 }
