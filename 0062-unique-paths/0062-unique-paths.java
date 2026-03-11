74af1f3794f31015330c9df3a51a24840d22e80f
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-03-11 14:39:40 +0530

    Time: 1 ms (14.45%), Space: 41.9 MB (90.44%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -26,27 +26,31 @@
 //         return dp[i][j];
 //     }
 // }
-
 class Solution {
     public int uniquePaths(int m, int n) {
 
-        int[][] dp = new int[2][n];
+        int[][] dp = new int[m][n];
 
-        for(int j = 0; j < n; j++){
-            dp[0][j] = 1;
-            dp[1][j] = 1;
-        }
+        // base case
+        dp[0][0] = 1;
 
-        for(int i = 1; i <=m-1; i++){
-            for(int j = 1; j < n; j++){
-                dp[1][j] = dp[1][j-1] + dp[0][j];
-            }
-
+        for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                dp[0][j] = dp[1][j];
+
+                if(i == 0 && j == 0) continue;
+
+                int up = 0;
+                int left = 0;
+
+                if(i > 0) up = dp[i-1][j];
+                if(j > 0) left = dp[i][j-1];
+
+                dp[i][j] = up + left;
             }
         }
 
-        return dp[1][n-1];
+        return dp[m-1][n-1];
     }
 }
+
+//fill last row and column by 1 as base case for tabulation
