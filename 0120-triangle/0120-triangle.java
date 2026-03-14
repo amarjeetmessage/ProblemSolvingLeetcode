2b47be3015fa34edc98a1fe31d3a7b0b2ca14f6a
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-03-14 13:47:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -54,51 +54,57 @@
 
 
 
-//Tabulation....
-class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size(); 
-        int[][] dp = new int[n][];
-        for(int i = 0; i< n; i++){
-            dp[i] = new int[triangle.get(i).size()];
-            Arrays.fill(dp[i], 0);
-        }
-
-        for(int j = 0; j< n; j++){
-            dp[n-1][j] = triangle.get(n-1).get(j);
-        }
-
-        for(int i = n-2; i >= 0; i--){
-            for(int j = 0; j<= i; j++){
-                int down = triangle.get(i).get(j) + dp[i+1][j];
-                int diagonal = triangle.get(i).get(j) + dp[i+1][j+1];
-
-                dp[i][j] = Math.min(down, diagonal);
-            }
-        }
-        return dp[0][0];
-    }
-}
-// Tabulation with size optimization.....
-
+// //Tabulation....
 // class Solution {
 //     public int minimumTotal(List<List<Integer>> triangle) {
-//         int n = triangle.size();
-//         int[] prev = new int[n];
+//         int n = triangle.size(); 
+//         int[][] dp = new int[n][];
 //         for(int i = 0; i< n; i++){
-//             prev[i] = triangle.get(n-1).get(i);
+//             dp[i] = new int[triangle.get(i).size()];
+//             Arrays.fill(dp[i], 0);
 //         }
 
-//         for(int i = n-2; i>= 0; i--){
-//             int[] temp = new int[triangle.get(i).size()];
-//             for(int j = 0; j<= i; j++){
-//                 int down = triangle.get(i).get(j) + prev[j];
-//                 int diagonal = triangle.get(i).get(j) + prev[j+1];
-//                 temp[j] = Math.min(down, diagonal);
-//             }
-//             prev = new int[triangle.get(i).size()];
-//             prev = temp;
+//         for(int j = 0; j< n; j++){
+//             dp[n-1][j] = triangle.get(n-1).get(j);
 //         }
-//         return prev[0];
+
+//         for(int i = n-2; i >= 0; i--){
+//             for(int j = 0; j<= i; j++){
+//                 int down = triangle.get(i).get(j) + dp[i+1][j];
+//                 int diagonal = triangle.get(i).get(j) + dp[i+1][j+1];
+
+//                 dp[i][j] = Math.min(down, diagonal);
+//             }
+//         }
+//         return dp[0][0];
 //     }
 // }
+
+
+
+
+
+
+Tabulation with size optimization.....
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] prev = new int[n];
+        for(int i = 0; i< n; i++){
+            prev[i] = triangle.get(n-1).get(i);
+        }
+
+        for(int i = n-2; i>= 0; i--){
+            int[] temp = new int[triangle.get(i).size()];
+            for(int j = 0; j<= i; j++){
+                int down = triangle.get(i).get(j) + prev[j];
+                int diagonal = triangle.get(i).get(j) + prev[j+1];
+                temp[j] = Math.min(down, diagonal);
+            }
+            prev = new int[triangle.get(i).size()];
+            prev = temp;
+        }
+        return prev[0];
+    }
+}
