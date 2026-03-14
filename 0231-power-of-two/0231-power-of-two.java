a6228ec94f7fee5a55cae23fd7de21b7d4764fc3
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-03-14 14:24:10 +0530

    Time: 1 ms (96.25%), Space: 42.3 MB (93.27%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,14 +1,5 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        // Base case: 1 is 2^0, so it’s valid
-        if (n == 1) {
-            return true;
-        }
-        // If n <= 0 or n is odd, it cannot be a power of two (except 1)
-        if (n <= 0 || n % 2 != 0) {
-            return false;
-        }
-        // Recursive step: divide by 2 and check again
-        return isPowerOfTwo(n / 2);
+        return n > 0 && (n & (n - 1)) == 0;
     }
 }
