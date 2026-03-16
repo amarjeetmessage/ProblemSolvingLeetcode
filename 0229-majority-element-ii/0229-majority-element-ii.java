cabab207d5773410ef179a206d84049ac8fdd0f5
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-03-16 16:54:39 +0530

    Time: 14 ms (43.54%), Space: 51.1 MB (95.66%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,18 +1,19 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n = nums.length;
-        HashMap<Integer, Integer> map = new HashMap<>();
         List<Integer> result = new ArrayList<>();
+        int n = nums.length;
+        int count = n/3;
 
-        // Count frequency of each number
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i = 0; i < n; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
 
-        // Collect elements that appear more than n/3 times
-        for (Integer key : map.keySet()) {
-            if (map.get(key) > n / 3) {
-                result.add(key);
+        // Check which elements appear more than n/3 times
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            if(entry.getValue() > count){
+                result.add(entry.getKey());
             }
         }
 
