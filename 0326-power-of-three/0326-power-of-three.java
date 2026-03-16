69aed4784b6ffc4a7f5c229e9654bcbaf56e13a3
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-03-16 16:14:06 +0530

    Time: 8 ms (91.93%), Space: 46.2 MB (41.76%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,14 +1,9 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        // Base case: 1 is 3^0, so it’s valid
-        if (n == 1) {
-            return true;
-        }
-        // If n <= 0 or n is odd, it cannot be a power of Three (except 1)
-        if (n <= 0 || n % 3 != 0) {
-            return false;
-        }
-        // Recursive step: divide by 3 and check again
-        return isPowerOfThree(n / 3);
+        
+        if(n == 1) return true;
+        if(n <= 0 || n % 3 != 0) return false;
+
+        return isPowerOfThree(n/3);
     }
 }
