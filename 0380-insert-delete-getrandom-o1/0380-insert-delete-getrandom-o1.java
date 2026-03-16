255e7d58628dd3f712f04976f0890b3fa7535ece
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-03-16 16:58:03 +0530

    Time: 34 ms (23.98%), Space: 99.3 MB (83.95%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -1,39 +1,41 @@
-import java.util.*;
+
 
 class RandomizedSet {
+    private ArrayList<Integer> list;
     private Map<Integer, Integer> map;
-    private List<Integer> list;
-    private Random rand;
 
     public RandomizedSet() {
-        map = new HashMap<>();
         list = new ArrayList<>();
-        rand = new Random();
+        map = new HashMap<>();
+    }
+
+    public boolean search(int val) {
+        return map.containsKey(val);
     }
 
     public boolean insert(int val) {
-        if (map.containsKey(val)) {
-            return false;
-        }
-        map.put(val, list.size());
+        if (search(val)) return false;
+
         list.add(val);
+        map.put(val, list.size() - 1);
         return true;
     }
 
     public boolean remove(int val) {
-        if (!map.containsKey(val)) {
-            return false;
-        }
+        if (!search(val)) return false;
+
         int index = map.get(val);
-        int lastElement = list.get(list.size() - 1);
-        list.set(index, lastElement);
-        map.put(lastElement, index);
+        list.set(index, list.get(list.size() - 1));
+        map.put(list.get(index), index);
         list.remove(list.size() - 1);
         map.remove(val);
+
         return true;
     }
 
     public int getRandom() {
+        Random rand = new Random();
         return list.get(rand.nextInt(list.size()));
     }
 }
+
