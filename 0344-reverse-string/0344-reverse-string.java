e48a973d164fd3d5fa22b40268481a6e45d3ed96
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-04-05 20:18:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,45 +1,17 @@
 class Solution {
     public void reverseString(char[] s) {
-        
-        // ================= Iterative Two-Pointer Approach (Uncommented - Ready for Submission) =================
-        int left = 0, right = s.length - 1;
-        
-        while (left < right) {
-            // swap
-            char temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
-            
-            left++;
-            right--;
-        }
+       int len = s.length;
+       int i = 0;
+       int j = len - 1;
+       
+       //apply while loop
+       while(i<j) {
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+
+        i+=1;
+        j-=1;
+       }
     }
-    
-    
-    /* ================= Recursive Approach (Commented) =================
-    public void reverseString(char[] s) {
-        helper(s, 0, s.length - 1);
-    }
-    
-    private void helper(char[] s, int left, int right) {
-        if (left >= right) return;
-        
-        char temp = s[left];
-        s[left] = s[right];
-        s[right] = temp;
-        
-        helper(s, left + 1, right - 1);
-    }
-    */
-    
-    
-    /* ================= For Loop Swap Approach (Commented) =================
-    public void reverseString(char[] s) {
-        for (int i = 0; i < s.length / 2; i++) {
-            char temp = s[i];
-            s[i] = s[s.length - 1 - i];
-            s[s.length - 1 - i] = temp;
-        }
-    }
-    */
 }
