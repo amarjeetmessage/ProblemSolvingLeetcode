8d94c2c665ebea381865a3e46b27524a33b56266
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-04-12 01:24:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,11 +1,35 @@
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         int len = nums.length;
+//         for(int i = 0; i<len; i++) {
+//             if(nums[i] == target) {
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+
 class Solution {
     public int search(int[] nums, int target) {
-        int len = nums.length;
-        for(int i = 0; i<len; i++) {
-            if(nums[i] == target) {
-                return i;
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } 
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            } 
+            else {
+                high = mid - 1;
             }
         }
+
         return -1;
     }
 }
