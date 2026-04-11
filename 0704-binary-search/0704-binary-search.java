3452b8756341a0bc2580639b6bb7c81deb81d913
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-04-12 01:16:17 +0530

    Time: 1 ms (9.09%), Space: 48.4 MB (38.31%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,23 +1,11 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start = 0, end = nums.length - 1;
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            }
-            if (nums[mid] < target) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+        int len = nums.length;
+        for(int i = 0; i<len; i++) {
+            if(nums[i] == target) {
+                return i;
             }
         }
         return -1;
-    }
-
-    public static void main(String[] args) {
-        int nums[] = {-7, -4, -1, 0, 3, 5, 9, 12};
-        int target = 0;
-        System.out.println("Index of key is : " + new Solution().search(nums, target));  // Corrected method call
     }
 }
