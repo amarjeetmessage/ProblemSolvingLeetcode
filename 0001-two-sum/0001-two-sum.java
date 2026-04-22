5ab60c5b12e37df1fc85e66f1a79cd1e59b3b36b
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-04-22 12:16:15 +0530

    Time: 51 ms (6.31%), Space: 47.2 MB (42.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,32 +1,15 @@
-// class Solution {
-//     public int[] twoSum(int[] nums, int target) {  
-//         for(int i = 0; i<nums.length; i++) {
-//             for(int j = i+1; j<nums.length; j++) {
-//                 if(nums[i] + nums[j] == target) {
-//                     return new int[]{i,j};
-//                 }
-//             }
-//         }
-//         return new int[]{};
-//     }
-// }// TC = O(n^2) AND SC = O(1);
-
-
-//SOLVED USING TWO POINTER TC & SC = O(n)
 class Solution {
-    public int[] twoSum(int[] nums, int target) {  
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for(int i = 0; i< nums.length; i++) {
-            int lookingFor = target - nums[i];
-
-            if(map.containsKey(lookingFor)) {
-                return new int[] {i, map.get(lookingFor)};
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        int[] ans = new int[2];
+        for(int i = 0; i<n; i++){
+            for(int j = i+1; j<n; j++){
+                if(nums[i] + nums[j] == target) {
+                    ans[0] = i;
+                    ans[1] = j;
+                }
             }
-            map.put(nums[i],i);
         }
-
-        return new int[] {};
+        return ans;
     }
 }
-
