88f2ef2fd53ad4034ad5e7777ea29736338dcae1
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-04-22 11:31:14 +0530

    Time: 2 ms (98.35%), Space: 44.1 MB (51.86%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
@@ -1,39 +1,14 @@
-// class Solution {
-//     public int countPairs(List<Integer> nums, int target) {
-//         int count = 0;
-        
-//         for(int i = 0; i < nums.size(); i++) {
-//             for(int j = i + 1; j < nums.size(); j++) {
-//                 if(nums.get(i) + nums.get(j) < target) {
-//                     count++;
-//                }
-//             }
-//         }
-//         return count;
-//     }
-// }
-
-
-
-
-//solve optimize using two pointer 
 class Solution {
     public int countPairs(List<Integer> nums, int target) {
-        nums.sort(null);
-
-        int i = 0;
-        int j = nums.size() -1;
-
+        int n = nums.size();
         int count = 0;
-
-        while(i<j) {
-            int sum = nums.get(i) + nums.get(j);
-            if(sum < target) {
-                count += j-i;
-                i = i+1;
-
-            }else{
-                j = j-1;
+        for(int i = 0; i<n; i++){
+            for(int j = i+ 1; j<n; j++){
+                if(nums.get(i) + nums.get(j) < target) {
+                    count++;
+                }else{
+                    continue;
+                }
             }
         }
         return count;
