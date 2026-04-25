97d72e06f68f4cd84c3a7ce7638d5fe23a50801f
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-04-25 16:47:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -1,4 +1,4 @@
-// /* The isBadVersion API is defined in the parent class VersionControl.
+// /* The isBadVersion APi is defined in the parent class VersionControl.
 //       boolean isBadVersion(int version); */
 
 // public class Solution extends VersionControl {
@@ -7,7 +7,7 @@
 //         int j = n;
 //         int ans = -1;
 //         while(i<=j){
-//             int mid = i+(j-i)/2;//In this question make sure to use this otherwise (i+j)/2 will overflow due to large number
+//             int mid = i+(j-i)/2;//in this question make sure to use this otherwise (i+j)/2 will overflow due to large number
 
 //             if(isBadVersion(mid) == true){
 //                 ans = mid;
@@ -25,23 +25,23 @@
 
 
 //TYPE 2 METHOD
-/* The isBadVersion API is defined in the parent class VersionControl.
+/* The isBadVersion APi is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int i = 1;
-        int j = n;
-        while(i<j){
-            int mid = i+(j-i)/2;//In this question make sure to use this otherwise (i+j)/2 will overflow due to large number
+        int left = 1;
+        int right = n;
+        while(left < right){
+            int mid = left + (right - left)/2;//in this question make sure to use this otherwise (i+j)/2 will overflow due to large number
 
             if(isBadVersion(mid) == true){
-                j = mid;
+                right = mid;
             }
             else{
-                i = mid+1;
+                left = mid+1;
             }
         }
-        return i;
+        return left;
     }
 }
