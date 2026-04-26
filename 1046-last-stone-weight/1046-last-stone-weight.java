d3af3d146546de2fd9bb319edf6646da711919ad
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-04-26 20:53:33 +0530

    Time: 1 ms (99.45%), Space: 42.7 MB (85.46%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,26 +1,26 @@
-class Solution {
-    public int lastStoneWeight(int[] stones) {
-        int n = stones.length;
-        //arraylist to modify 
-        List<Integer> arr = new ArrayList<>();
-        for(int i = 0; i<n; i++){
-            arr.add(stones[i]);
-        }
+// class Solution {
+//     public int lastStoneWeight(int[] stones) {
+//         int n = stones.length;
+//         //arraylist to modify 
+//         List<Integer> arr = new ArrayList<>();
+//         for(int i = 0; i<n; i++){
+//             arr.add(stones[i]);
+//         }
 
-        while (arr.size() > 1) {
-            Collections.sort(arr);
-            int a = arr.remove(arr.size() - 1);
-            int b = arr.remove(arr.size() - 1);
+//         while (arr.size() > 1) {
+//             Collections.sort(arr);
+//             int a = arr.remove(arr.size() - 1);
+//             int b = arr.remove(arr.size() - 1);
 
-            if(a > b){
-                arr.add(a - b);
-            }
-        }
-        if(arr.size() == 1){
-            return arr.get(0);
-        }else return 0;
-    }
-}
+//             if(a > b){
+//                 arr.add(a - b);
+//             }
+//         }
+//         if(arr.size() == 1){
+//             return arr.get(0);
+//         }else return 0;
+//     }
+// }
 
 
 
@@ -54,8 +54,24 @@ class Solution {
 
 // //USING HEAPS PATTERN 
 
-// class Solution {
-//     public int lastStoneWeight(int[] stones) {
-        
-//     }
-// }
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i = 0; i<stones.length; i++){
+            pq.offer(stones[i]);
+        }
+
+        while (pq.size() > 1) {
+            int a = pq.poll();
+            int b = pq.poll();
+
+            if(a > b){
+                pq.offer(a - b);
+            }
+        }
+        if(pq.size() == 1){
+            return pq.peek();
+        }else return 0;
+    }
+}
