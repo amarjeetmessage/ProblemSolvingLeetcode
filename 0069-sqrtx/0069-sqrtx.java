ea10db9f001da9fbf1f401796c828cd6944d2baa
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-04-27 18:41:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -23,24 +23,48 @@
 
 
 
+
 class Solution {
     public int mySqrt(int x) {
-        if (x == 0 || x == 1) return x;
-
-        int left = 1, right = x;
-        int ans = 0;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (mid <= x / mid) {
-                ans = mid;        // possible answer
-                left = mid + 1;   // try bigger
-            } else {
-                right = mid - 1;  // go smaller
+        // if(x == 2147483647) return 46340;
+        for(long i = 0; i< (x/2) + 2; i++){
+            if(i*i == x){
+                return (int)i;
+            }
+            else if(i*i > x){
+                return (int)(i-1);
             }
         }
-
-        return ans;
+        return -1;
     }
 }
+
+
+
+
+
+
+
+
+// //Using Binary Search 
+// class Solution {
+//     public int mySqrt(int x) {
+//         if (x == 0 || x == 1) return x;
+
+//         int left = 1, right = x;
+//         int ans = 0;
+
+//         while (left <= right) {
+//             int mid = left + (right - left) / 2;
+
+//             if (mid <= x / mid) {
+//                 ans = mid;        // possible answer
+//                 left = mid + 1;   // try bigger
+//             } else {
+//                 right = mid - 1;  // go smaller
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
