f38f1dc5008812725f630c15803a1a354eba2483
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-04-27 23:04:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,9 +1,43 @@
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         for(int i = 0; i<nums.length;i++){
+//             if(nums[i] == target){
+//                 return i;
+//             }
+//         }return -1;
+//     }
+// }
+
+
+
+//SOLVING BY BINARY SEARCHING 
 class Solution {
     public int search(int[] nums, int target) {
-        for(int i = 0; i<nums.length;i++){
-            if(nums[i] == target){
-                return i;
+        int low = 0;
+        int high = nums.length -1;
+
+        while(low <= high) {
+            int mid = low + (high - low ) / 2;
+
+            if(nums[mid] == target){
+                return mid;
             }
-        }return -1;
+
+            if(nums[low] <= nums[mid]){
+                if(target >= nums[low] && target <= nums[mid]){
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
+            }else{
+                if(target >= nums[mid] && target <= nums[high]){
+                    low = mid + 1;
+                }else{
+                    high = mid -1;
+                }
+            }
+        }
+        return -1;
     }
 }
+
