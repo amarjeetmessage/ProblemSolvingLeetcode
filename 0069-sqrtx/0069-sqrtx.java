22d43520e68d3c53318bd1e8f82468bd30973e53
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-04-27 18:25:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -5,38 +5,42 @@
 //     }
 // }
 
-class Solution {
-    public int mySqrt(int x) {
-        if(x == 2147483647) return 46340;
-        for(int i = 0; i< (x/2) + 2; i++){
-            if(i*i == x){
-                return i;
-            }
-            else if(i*i > x){
-                return i-1;
-            }
-        }
-        return -1;
-    }
-}
 // class Solution {
 //     public int mySqrt(int x) {
-//         if (x == 0 || x == 1) return x;
-
-//         int left = 1, right = x;
-//         int ans = 0;
-
-//         while (left <= right) {
-//             int mid = left + (right - left) / 2;
-
-//             if (mid <= x / mid) {
-//                 ans = mid;        // possible answer
-//                 left = mid + 1;   // try bigger
-//             } else {
-//                 right = mid - 1;  // go smaller
+//         if(x == 2147483647) return 46340;
+//         for(int i = 0; i< (x/2) + 2; i++){
+//             if(i*i == x){
+//                 return i;
+//             }
+//             else if(i*i > x){
+//                 return i-1;
 //             }
 //         }
-
-//         return ans;
+//         return -1;
 //     }
 // }
+
+
+
+
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0 || x == 1) return x;
+
+        int left = 1, right = x;
+        int ans = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (mid <= x / mid) {
+                ans = mid;        // possible answer
+                left = mid + 1;   // try bigger
+            } else {
+                right = mid - 1;  // go smaller
+            }
+        }
+
+        return ans;
+    }
+}
