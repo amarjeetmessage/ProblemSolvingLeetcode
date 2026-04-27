8eef85aec1e1360bd2c07e69d30eeda9196f58f0
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-04-27 17:59:57 +0530

    Time: 1 ms (97.86%), Space: 42.3 MB (94.87%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,27 +1,27 @@
-// class Solution {
-//     public int mySqrt(int x) {
-//         return (int)Math.sqrt(x);
-
-//     }
-// }
 class Solution {
     public int mySqrt(int x) {
-        if (x == 0 || x == 1) return x;
+        return (int)Math.sqrt(x);
 
-        int left = 1, right = x;
-        int ans = 0;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (mid <= x / mid) {
-                ans = mid;        // possible answer
-                left = mid + 1;   // try bigger
-            } else {
-                right = mid - 1;  // go smaller
-            }
-        }
-
-        return ans;
     }
 }
+// class Solution {
+//     public int mySqrt(int x) {
+//         if (x == 0 || x == 1) return x;
+
+//         int left = 1, right = x;
+//         int ans = 0;
+
+//         while (left <= right) {
+//             int mid = left + (right - left) / 2;
+
+//             if (mid <= x / mid) {
+//                 ans = mid;        // possible answer
+//                 left = mid + 1;   // try bigger
+//             } else {
+//                 right = mid - 1;  // go smaller
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
