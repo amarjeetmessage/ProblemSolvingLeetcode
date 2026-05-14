603099b95e969dd5e6b5f99650a94721b4e57f80
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-05-15 01:57:43 +0530

    Time: 4 ms (52.76%), Space: 45.2 MB (31.62%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.java
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.java
@@ -1,21 +1,12 @@
 class Solution {
     public boolean isGood(int[] nums) {
         int n = nums.length - 1;
-        boolean dup = false;
-
-        for (int num : nums) {
-            int val = Math.abs(num);
-            if (val > n) return false;
-
-            if (nums[val - 1] < 0) {
-                if (val < n || dup) return false;
-                dup = true;
-                continue;
+        Arrays.sort(nums);
+        for(int i = 0; i<n; i++){
+            if(nums[i] != i+1){
+                return false;
             }
-
-            nums[val - 1] = -nums[val - 1];
         }
-
-        return true;
+        return nums[n] == n;
     }
 }
