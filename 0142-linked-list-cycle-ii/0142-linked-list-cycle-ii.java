b4fb013d33513f610fb54415183ab9b92edc7d15
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-02 20:52:48 +0530

    Time: 0 ms (100%), Space: 46.2 MB (98.11%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,15 +11,45 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        HashSet<ListNode> set = new HashSet<>();
 
-        while(head != null) {
-            if(set.contains(head)){
-                return head;
-            }
-            set.add(head);
-            head = head.next;
+        // // brute force using hashset 
+        // HashSet<ListNode> set = new HashSet<>();
+
+        // while(head != null) {
+        //     if(set.contains(head)){
+        //         return head;
+        //     }
+        //     set.add(head);
+        //     head = head.next;
+        // }
+        // return null;
+
+        // // optimized approach using floyd's cycle detection using slow and fast approach
+
+        if(head == null) return null;
+        // if(head.next == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast) break;
+
         }
-        return null;
+
+        if(head.next == null) return null;
+        if(slow != fast ) return null;
+
+        slow = head;
+
+        while(fast != slow ) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        return fast;
+
     }
 }
