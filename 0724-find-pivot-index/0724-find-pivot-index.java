4726e99d1ae5a465427acf6fe751b7d2b2a39f72
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-05 08:01:34 +0530

    Time: 1 ms (98.63%), Space: 47.4 MB (59.42%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n =nums.length;
+        if(n == 1) return 0;
+        // if(n == 2) return -1;
+        int sum = 0;
+        for(int i =0; i<n; i++){
+            sum = sum + nums[i];
+        }
+
+        int hsum = 0;
+        for(int i = 0; i<n; i++){
+            
+            if(hsum *2 + nums[i] == sum){
+                return i;
+            }
+
+            else {
+                hsum = hsum + nums[i];
+            }
+        }
+        return -1;
+    }
+}
