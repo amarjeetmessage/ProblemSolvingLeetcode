dd7e2283f80fb1830504154e7fbbbb5e00015371
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-07 21:17:34 +0530

    Time: 0 ms (100%), Space: 48.4 MB (31.19%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,27 +1,3 @@
-class Solution {
-    public int search(int[] nums, int target) {
-      //apply binary search 
-      int n = nums.length;
-      int left = 0;
-      int right = n-1;
-
-      while(left <= right){
-        int mid = (left + right) / 2;
-
-        if(nums[mid] == target){
-            return mid;
-        }
-        else if(nums[mid] > target){
-            right = mid-1;
-        }else{
-            left = mid+1;
-        }
-      } 
-      return -1; 
-    }
-}
-
-
 // class Solution {
 //     public int search(int[] nums, int target) {
 //         int len = nums.length;
@@ -35,25 +11,25 @@ class Solution {
 // }
 
 
-// class Solution {
-//     public int search(int[] nums, int target) {
-//         int low = 0;
-//         int high = nums.length - 1;
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
 
-//         while (low <= high) {
-//             int mid = low + (high - low) / 2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
 
-//             if (nums[mid] == target) {
-//                 return mid;
-//             } 
-//             else if (nums[mid] < target) {
-//                 low = mid + 1;
-//             } 
-//             else {
-//                 high = mid - 1;
-//             }
-//         }
+            if (nums[mid] == target) {
+                return mid;
+            } 
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            } 
+            else {
+                high = mid - 1;
+            }
+        }
 
-//         return -1;
-//     }
-// }
+        return -1;
+    }
+}
