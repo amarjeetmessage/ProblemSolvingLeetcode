a36cd96a4f415e97d531d79fadaac1fa256fe2b6
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-07 17:10:09 +0530

    Time: 15 ms (55.4%), Space: 47.6 MB (65.93%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,42 +1,30 @@
 class Solution {
-
     public int[] topKFrequent(int[] nums, int k) {
-
-        // Step 1: Store frequency of every number
+        int n = nums.length;
+        //create hasmap
         HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int num : nums) {
+        // hasmap store all value
+        for(int num : nums){
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        // Step 2: Min Heap based on frequency
-        PriorityQueue<Map.Entry<Integer, Integer>> pq =
-                new PriorityQueue<>(
-                        (a, b) -> a.getValue() - b.getValue()
-                );
-
-        // Step 3: Insert every entry into heap
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-
+        //create PriorityQueue of of hashMap type
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>( (a,b) -> a.getValue() - b.getValue());
+        // keep only k elements in pq
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
             pq.offer(entry);
 
-            // Keep only k elements
-            if (pq.size() > k) {
+            if(pq.size() > k) {
                 pq.poll();
             }
         }
 
-        // Step 4: Store answer
-        int[] ans = new int[k];
-
-        // Remove elements from heap
-        // for (int i = k - 1; i >= 0; i--) {
-        //     ans[i] = pq.poll().getKey();
-        // }
+        //create arr of k size to store ans
+        int ans[] = new int[k];
+        // use for loop to add key of pq in arr
         for(int i = 0; i<k; i++){
             ans[i] = pq.poll().getKey();
         }
-
         return ans;
     }
 }
