0cab4de1b8f5c006caf275430c5a45e9f8d494dd
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-08 16:52:29 +0530

    Time: 0 ms (100%), Space: 52.9 MB (38.62%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,11 +1,48 @@
+// class Solution {
+//     public int singleNonDuplicate(int[] nums) {
+//         int n = nums.length;
+//         for (int i = 0; i < n - 1; i += 2) {
+//             if (nums[i] != nums[i + 1]) {
+//                 return nums[i];
+//             }
+//         }
+//         return nums[n - 1]; // handle last element
+//     }
+// }
+
+
+
+
 class Solution {
     public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
 
-        for(int i = 0; i<nums.length-1; i = i+2){
-            if(nums[i] != nums[i+1]){
-                return nums[i];
-            }            
+        if (n == 1) return nums[0];
+        if (nums[0] != nums[1]) return nums[0];
+        if (nums[n - 1] != nums[n - 2]) return nums[n - 1];
+
+        int low = 1;
+        int high = n - 2;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            // Found the single element
+            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+                return nums[mid];
+            }
+
+            // We are on the left (correct pairing)
+            if ((mid % 2 == 0 && nums[mid] == nums[mid + 1]) ||
+                (mid % 2 == 1 && nums[mid] == nums[mid - 1])) {
+                low = mid + 1;
+            }
+            // We are on the right side
+            else {
+                high = mid - 1;
+            }
         }
-        return nums[nums.length -1];
+
+        return -1;
     }
 }
