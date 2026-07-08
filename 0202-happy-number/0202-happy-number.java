09bfe7b1b007dc8e175a91cd1b630bd3aa609424
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-08 10:18:09 +0530

    Time: 1 ms (78.23%), Space: 42.8 MB (12.09%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int nextDigit(int n) {
+    int sum = 0;
+        while(n > 0){
+            int digit = n % 10;
+            sum = sum + (digit * digit);
+            n = n / 10;
+        }
+        return sum;
+    }
+    public boolean isHappy(int n) {
+       HashSet<Integer> set = new HashSet<>();
+
+       while(n != 1){
+
+            if(set.contains(n)){
+                return false;
+            }
+
+            set.add(n);
+
+            n = nextDigit(n);
+       }
+       return true;
+    }
+}
