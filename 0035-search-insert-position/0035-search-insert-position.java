686bcfd5dadffe98fa15a61f0c9c7e5aa56da595
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-08 14:56:09 +0530

    Time: 0 ms (100%), Space: 44.6 MB (66.36%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,7 +1,7 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int n = nums.length;
-        if(nums[0] >= target){
+        if(nums[0] > target){
             return 0;
         }
         if(nums[n-1] < target) return n;
