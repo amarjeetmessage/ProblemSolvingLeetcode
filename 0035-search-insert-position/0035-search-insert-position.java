13a7e523f23e9877cc6e8c19b331294ec61140f4
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-08 15:08:04 +0530

    Time: 0 ms (100%), Space: 44.7 MB (47.96%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,18 +1,44 @@
+// class Solution {
+//     public int searchInsert(int[] nums, int target) {
+//         int n = nums.length;
+//         if(nums[0] > target){
+//             return 0;
+//         }
+//         if(nums[n-1] < target) return n;
+
+//         for(int i = 0; i<n; i++){
+//             if(nums[i] == target) return i;
+
+//             if(nums[i]< target && nums[i+1] > target){
+//                 return i+1;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+
+
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int n = nums.length;
-        if(nums[0] > target){
-            return 0;
-        }
-        if(nums[n-1] < target) return n;
+        
+        int low = 0;
+        int high = n-1;
 
-        for(int i = 0; i<n; i++){
-            if(nums[i] == target) return i;
+        while(low <= high) {
+            int mid = (low + high) / 2;
 
-            if(nums[i]< target && nums[i+1] > target){
-                return i+1;
+            if(nums[mid] == target){
+                return mid;
+            }
+
+            if(nums[mid] < target){
+                low = mid+1;
+            }else{
+                high = mid - 1;
             }
         }
-        return -1;
+        return low ;
     }
 }
