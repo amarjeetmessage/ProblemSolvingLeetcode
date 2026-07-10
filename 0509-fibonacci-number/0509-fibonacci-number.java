ae240d59bc794c886691195aa76a5d54319af951
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-10 10:00:29 +0530

    Time: 0 ms (100%), Space: 42.2 MB (15.76%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,6 +1,22 @@
 class Solution {
+    public int solve(int n, int dp[]) {
+        //base case
+        if(n == 0 || n == 1) {
+            return n;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        // recursive call
+
+       return dp[n] = solve(n-1,dp) + solve(n-2, dp);
+
+        // return result
+        // return dp[n];
+    }
     public int fib(int n) {
-        if(n == 0 || n == 1) return n;
-        return fib(n-1) + fib(n-2);
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+       return solve(n, dp);
     }
 }
