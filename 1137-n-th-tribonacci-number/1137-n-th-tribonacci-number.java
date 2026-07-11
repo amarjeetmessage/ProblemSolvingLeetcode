018c1afecb2fd3195df144a336eeef4341e60355
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-11 09:13:47 +0530

    Time: 0 ms (100%), Space: 42 MB (52.8%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,22 +1,19 @@
 class Solution {
-
-    public static int findFibb(int n, int[] dp) {
-        //base case 
-        if(n == 0) return n;
+    public int solve(int n,int[] dp) {
+        if(n == 0) return 0;
         if(n == 1 || n == 2) return 1;
-        
-        //check first if it exists
-        if(dp[n] != -1) {
+
+        if(dp[n] != -1){
             return dp[n];
         }
 
-        dp[n] = findFibb(n-1, dp) + findFibb(n-2, dp) + findFibb(n-3, dp);
+        dp[n] = solve(n-1,dp) + solve(n-2,dp) + solve(n-3,dp);
+
         return dp[n];
     }
     public int tribonacci(int n) {
         int[] dp = new int[n+1];
         Arrays.fill(dp, -1);
-
-        return findFibb(n, dp);
+        return solve(n, dp);
     }
 }
