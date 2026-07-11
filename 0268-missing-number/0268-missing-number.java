94d6209cc9090a5aeb2b6f8673f29fde579761b8
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-11 16:21:24 +0530

    Time: 5 ms (21.62%), Space: 47.2 MB (65.67%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,11 +1,30 @@
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         int res = nums.length;
+        
+//         for (int i = 0; i < nums.length; i++) {
+//             res += i - nums[i];
+//         }
+        
+//         return res;       
+//     }
+// }
+
+
+// without sorting and linear search or binary search or without using HashSet use of cyclic sorting to solve in O(n);
+
 class Solution {
     public int missingNumber(int[] nums) {
-        int res = nums.length;
-        
-        for (int i = 0; i < nums.length; i++) {
-            res += i - nums[i];
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums) {
+            set.add(num);
         }
-        
-        return res;       
+        for(int i = 0; i<nums.length;i++){
+            if(!set.contains(i)){
+                return i;
+            }
+        }
+        return nums.length;
     }
 }
+
