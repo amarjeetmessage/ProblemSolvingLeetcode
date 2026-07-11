6362ad93d347ba8b785b19c1397a21396796685a
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-11 20:10:23 +0530

    Time: 6 ms (81.35%), Space: 67 MB (74.45%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,19 +1,52 @@
+// class Solution {
+//     public List<Integer> findDisappearedNumbers(int[] nums) {
+//        int n = nums.length;
+//        Set<Integer> set = new HashSet<>();
+
+//        for(int num : nums){
+//         set.add(num);
+//        }
+
+//         List<Integer> list = new ArrayList<>();
+//        for(int i = 1; i<=n; i++){
+//         if(!set.contains(i)){
+//             list.add(i);
+//         }
+//        }
+//        return list;
+
+//     }
+// }
+
+// SOLVING USING CYCLIC SORT IS BEST
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
        int n = nums.length;
-       Set<Integer> set = new HashSet<>();
+       int i = 0;
 
-       for(int num : nums){
-        set.add(num);
-       }
+       while(i < n) {
+        int correctIdx = nums[i] -1;
 
-        List<Integer> list = new ArrayList<>();
-       for(int i = 1; i<=n; i++){
-        if(!set.contains(i)){
-            list.add(i);
+        // sahi index par sahi element hai to loop avoid karne ke liye aage badho
+        if(nums[i] != nums[correctIdx]) {
+            //swap
+            int temp = nums[i];
+            nums[i] = nums[correctIdx];
+            nums[correctIdx] = temp;
+        }else{
+            i = i+1;
         }
        }
-       return list;
 
+       List<Integer> list = new ArrayList<>();
+
+        for (i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                list.add(i + 1);
+            }
+        }
+
+        return list;
+       
     }
 }
