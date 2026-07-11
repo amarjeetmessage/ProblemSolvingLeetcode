a8d762bf2c9caca2e8196d413e21d267076a73f5
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-11 18:57:36 +0530

    Time: 4 ms (90.75%), Space: 83.1 MB (28.52%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,13 +1,39 @@
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+//         Set<Integer> set = new HashSet<>();
+
+//         for(int num : nums) {
+//             if(set.contains(num) ) {
+//                 return num;
+//             }
+//             set.add(num);
+//         }
+//         return -1;
+//     }
+// }
+
+
+
+// we can also solve by cyclic sort but not good 
+
 class Solution {
     public int findDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>();
+        int slow = 0;
+        int fast = 0;
 
-        for(int num : nums) {
-            if(set.contains(num) ) {
-                return num;
-            }
-            set.add(num);
+        // Phase 1: Find intersection point
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // Phase 2: Find the entrance of the cycle (duplicate)
+        slow = 0; // or n1 = 0
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return -1;
+
+        return slow; // duplicate number
     }
 }
