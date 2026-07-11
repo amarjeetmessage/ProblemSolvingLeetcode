9b78d6ad7f450a4cacd345585c2b327b7615076b
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-11 16:45:27 +0530

    Time: 1 ms (29.85%), Space: 47.3 MB (65.67%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -11,20 +11,62 @@
 // }
 
 
-// without sorting and linear search or binary search or without using HashSet use of cyclic sorting to solve in O(n);
+
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         HashSet<Integer> set = new HashSet<>();
+//         for(int num : nums) {
+//             set.add(num);
+//         }
+//         for(int i = 0; i<nums.length;i++){
+//             if(!set.contains(i)){
+//                 return i;
+//             }
+//         }
+//         return nums.length;
+//     }
+// }
+
+
+
+// without sorting and linear search or binary search 
+// or without using HashSet which solve in O(n) but space complexity will be O(n)
+
+// use of cyclic sorting to solve in O(n) with constant space
 
 class Solution {
     public int missingNumber(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int num : nums) {
-            set.add(num);
-        }
-        for(int i = 0; i<nums.length;i++){
-            if(!set.contains(i)){
-                return i;
+        int n = nums.length;
+        int i = 0;
+        //in - place nums array ko modify karke 
+        //sahi index par pahuchate hai
+
+        //cyclic sorting
+        while(i < n) {
+            int correctIndex = nums[i];// due to starts with 0
+            
+            //exception
+            if(correctIndex >= nums.length) {
+                i = i+1;
+            }
+            else if(nums[correctIndex] != nums[i]) {
+                int temp = nums[correctIndex];
+                nums[correctIndex] = nums[i];
+                nums[i] = temp;
+            }
+
+            else {
+                i = i+1;
             }
         }
-        return nums.length;
+
+        for(int k = 0; k< n; k++){
+            if(nums[k] != k){
+                return k;
+            }
+        }
+        return n;
+
     }
 }
 
