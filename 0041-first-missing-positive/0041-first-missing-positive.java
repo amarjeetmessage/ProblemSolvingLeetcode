37bccffb65773b54f2bfd0a319ca12490e78f357
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-11 19:45:52 +0530

    Time: 1 ms (100%), Space: 71.1 MB (72.9%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,16 +1,51 @@
+// class Solution {
+//     public int firstMissingPositive(int[] nums) {
+//         int n = nums.length;
+//         Set<Integer> set = new HashSet<>();
+//         for(int num : nums) {
+//             set.add(num);
+//         }
+
+//         for(int i = 1; i<=n; i++){
+//             if(!set.contains(i)){
+//                 return i;
+//             }
+//         }
+//         return n+1;
+//     }
+// }
+
+
+
+// SOLVING USING CYCLIC SORT IS BEST
 class Solution {
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
-        Set<Integer> set = new HashSet<>();
-        for(int num : nums) {
-            set.add(num);
-        }
+        int i = 0;
 
-        for(int i = 1; i<=n; i++){
-            if(!set.contains(i)){
-                return i;
+        while(i<n){
+            int correctIdx = nums[i] -1;
+
+            if(correctIdx < 0 || correctIdx >= n){
+                i = i+1;
+            }
+            else if(nums[correctIdx] != nums[i]){
+                int temp = nums[correctIdx];
+                nums[correctIdx] = nums[i];
+                nums[i] = temp;
+            }else{
+                i = i+1;
             }
         }
-        return n+1;
+
+        // sorted now traverse array to find missing
+        for(i = 0; i<n; i++){
+            if(nums[i] != i+1){
+                return i+1;
+            }
+        }
+        return i+1;
+        
     }
 }
+
