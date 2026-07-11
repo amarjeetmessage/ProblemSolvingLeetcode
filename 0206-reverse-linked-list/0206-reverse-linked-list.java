c2b2bbade345c8162b51c2c6c837a52805ff5d06
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-11 23:19:05 +0530

    Time: 0 ms (100%), Space: 44.2 MB (49.9%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -1,3 +1,70 @@
+// solving using the brute force appraoch using the extra space
+//TC = O(n)
+//SC = O(n)
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         //for 1 or 0 number of nodes
+//         if(head == null || head.next == null) {
+//             return head;
+//         }
+
+//         // 1 -> 2 -> 3 -> 4
+//         // store in list as [ 4, 3, 2, 1 ]
+//         // 4 -> 3 -> 2 -> 1 -> null
+
+//         int len = 0;
+//         ListNode l = head;
+//         while(l != null) {
+//             l = l.next;
+//             len++;
+//         }
+//         //finded length of LL = 4
+
+//         // store in list 
+//         ListNode[] arr = new ListNode[len];
+//         //[null , null , null , null]
+
+
+//         //fill in array normally
+//         l = head;
+//         int i = 0;
+//         while(l != null) {
+//             arr[i] = l;
+//             i++;
+//             l = l.next;
+//         }
+//         //it stores nodes null value
+//         //[(1) , (2) , (3), (4)]
+
+//         //now reverse the list 
+//         int start = 0;
+//         int end = len -1 ;
+
+//         while(start < end) {
+//             ListNode temp = arr[start];
+//             arr[start] = arr[end];
+//             arr[end] = temp;
+
+//             start = start + 1;
+//             end = end -1 ;
+//         }
+//         //not look like as [(4), (3), (2), (1)];
+
+//         //not acc to list point
+//         for(int k = 0; k<len-1; k++) {//note that (len -1 )
+//             arr[k].next = arr[k+1];
+//         }
+//         arr[len - 1].next = null;//last node points to null
+
+//         return arr[0]; 
+//     }
+// }
+
+
+
+
+
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -8,63 +75,32 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+//solved using in-place-manupulation
+//TC = O(n)
+//SC = O(1)
 class Solution {
     public ListNode reverseList(ListNode head) {
-        //for 1 or 0 number of nodes
-        if(head == null || head.next == null) {
-            return head;
-        }
+    // ALGORITHM USING IN PLACE MANUPULATION LL
+        
+       //save next 
+       // reverse current link
+       //move all pointers farword 
 
-        // 1 -> 2 -> 3 -> 4
-        // store in list as [ 4, 3, 2, 1 ]
-        // 4 -> 3 -> 2 -> 1 -> null
+    ListNode prev = null;
+    ListNode curr = head;
 
-        int len = 0;
-        ListNode l = head;
-        while(l != null) {
-            l = l.next;
-            len++;
-        }
-        //finded length of LL = 4
+    while(curr != null) {
 
-        // store in list 
-        ListNode[] arr = new ListNode[len];
-        //[null , null , null , null]
+        ListNode next = curr.next;//save next 
 
+        //reverse the curr node
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+        
+    }
+    return prev;//prev because dry run it at last prev will be bahave as head of new node 
 
-        //fill in array normally
-        l = head;
-        int i = 0;
-        while(l != null) {
-            arr[i] = l;
-            i++;
-            l = l.next;
-        }
-        //it stores nodes null value
-        //[(1) , (2) , (3), (4)]
-
-        //now reverse the list 
-        int start = 0;
-        int end = len -1 ;
-
-        while(start < end) {
-            ListNode temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-
-            start = start + 1;
-            end = end -1 ;
-        }
-        //not look like as [(4), (3), (2), (1)];
-
-        //not acc to list point
-        for(int k = 0; k<len-1; k++) {//note that (len -1 )
-            arr[k].next = arr[k+1];
-        }
-        arr[len - 1].next = null;//last node points to null
-
-        return arr[0];
-
- 
     }
 }
