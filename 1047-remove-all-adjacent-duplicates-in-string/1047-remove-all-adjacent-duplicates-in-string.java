b827062bf793d4ea6c5c7ca9cac5de6c5d170cd2
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-12 14:05:31 +0530

    Time: 35 ms (44.75%), Space: 47.1 MB (66.2%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,18 +1,55 @@
+// class Solution {
+//     public String removeDuplicates(String s) {
+
+//         StringBuilder sb = new StringBuilder();
+
+//         for (char ch : s.toCharArray()) {
+
+//             int len = sb.length();
+
+//             if (len > 0 && sb.charAt(len - 1) == ch) {
+//                 sb.deleteCharAt(len - 1);   // Pop
+//             } else {
+//                 sb.append(ch);              // Push
+//             }}
+
+//         return sb.toString();
+//     }
+// }
+
+
+
 class Solution {
     public String removeDuplicates(String s) {
 
+        /*
+        PSEDO CODE
+
+        stack of character
+        iterate through the string
+        if(stack last char is equal to current char) then remove from stack otherwise push current char
+
+        keep popping ftome stack till it is empty and add it to a stringBuilder 
+
+        reverse stringbuilder and return as ans
+        */
+
+        int n = s.length();
+        Stack<Character> st = new Stack<>();
         StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<n; i++) {
+            if(!st.isEmpty() && st.peek() == s.charAt(i)){
+                st.pop();
+            }else{
+                st.push(s.charAt(i));
+            }
+        }
 
-        for (char ch : s.toCharArray()) {
+        while(!st.isEmpty()) {
+            sb.append(st.pop());
+        }
 
-            int len = sb.length();
-
-            if (len > 0 && sb.charAt(len - 1) == ch) {
-                sb.deleteCharAt(len - 1);   // Pop
-            } else {
-                sb.append(ch);              // Push
-            }}
-
-        return sb.toString();
+        // reverse StringBuilder sb 
+        return sb.reverse().toString();
     }
 }
