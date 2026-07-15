99dd3bcf970f2849fbe6d7f077062dede58f8f26
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-15 18:21:54 +0530

    Time: 0 ms (100%), Space: 42.7 MB (99.09%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -1,28 +1,61 @@
+// class Solution {
+//     public int countStudents(int[] students, int[] sandwiches) {
+//         Queue<Integer> q = new LinkedList<>();
+
+//         for(int student : students){
+//             q.offer(student);
+//         }
+
+//         int stuIndex = 0;
+//         int rejectCount = 0;
+
+//         while(!q.isEmpty()){
+//             if(q.peek() == sandwiches[stuIndex]){
+//                 q.poll();
+//                 rejectCount = 0;
+//                 stuIndex++;
+//             }else{
+//                 rejectCount++;
+//                 q.offer(q.poll());
+//             }
+
+//             if(rejectCount == q.size()){
+//                 return q.size();
+//             }
+//         }
+//         return 0;
+//     }
+// }
+
+
+
+
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
-        Queue<Integer> q = new LinkedList<>();
-
-        for(int student : students){
-            q.offer(student);
+        int n = students.length;
+        int count0 = 0;
+        int count1 = 0;
+        for(int i = 0; i<n;i++){
+            if(students[i] == 0){
+                count0++;
+            }else count1++;
         }
 
-        int stuIndex = 0;
-        int rejectCount = 0;
-
-        while(!q.isEmpty()){
-            if(q.peek() == sandwiches[stuIndex]){
-                q.poll();
-                rejectCount = 0;
-                stuIndex++;
-            }else{
-                rejectCount++;
-                q.offer(q.poll());
+        for(int i = 0; i<n; i++){
+            if(sandwiches[i] == 0){
+                if(count0 == 0){
+                    break;
+                }
+                count0--;
             }
-
-            if(rejectCount == q.size()){
-                return q.size();
+            else if(sandwiches[i] == 1){
+                if(count1 == 0){
+                    break;
+                }
+                count1--;
             }
         }
-        return 0;
+
+        return count0 + count1;
     }
 }
