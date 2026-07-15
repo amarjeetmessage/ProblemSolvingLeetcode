1f0fa3e1ab2cc0f372e1e2db9a8cb463bd3af619
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-15 23:05:26 +0530

    Time: 0 ms (100%), Space: 42.1 MB (33.5%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -26,26 +26,37 @@
 // }
 
 
-class Solution {
-    public int fib(int n) {
-        int[] dp = new int[n+1];
-        if(n == 0 ) return 0;
-        dp[0] = 0;
-        dp[1] = 1;
-
-        for(int i = 2; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-
-        return dp[n];
-    }
-
-}
-
-
-
 // class Solution {
 //     public int fib(int n) {
-        
+//         int[] dp = new int[n+1];
+//         if(n == 0 ) return 0;// need to handle due to if only 0 then can't dp[1] = 1;
+//         dp[0] = 0;
+//         dp[1] = 1;
+
+//         for(int i = 2; i<=n; i++){
+//             dp[i] = dp[i-1] + dp[i-2];
+//         }
+
+//         return dp[n];
 //     }
+
 // }
+
+
+
+class Solution {
+    public int fib(int n) {
+        int[] dp = new int[3];
+        if(n == 0) return n;
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for(int i = 3; i<=n; i++){
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+            dp[2] = dp[0] + dp[1];
+        }
+        return dp[2];
+    }
+}
