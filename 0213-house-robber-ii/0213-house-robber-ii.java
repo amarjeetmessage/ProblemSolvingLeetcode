c03cde1f14603975fbc4306cc52ea65068f1a17b
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-16 15:50:52 +0530

    Time: 0 ms (100%), Space: 43.2 MB (8.09%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,35 +1,68 @@
-class Solution {
-    public int rob1(int[] nums) {
-        int size = nums.length;
-        if(size == 0) return 0;
-        if(size == 1) return nums[0];
-        int prev2 = 0;
-        int prev1 = nums[0];
+// class Solution {
+//     public int solve(int[] nums, int end, int start) {
+//         if(end < start) return 0;
 
-        for(int i = 2;i<=size;i++){
-            int pick = nums[i-1] + prev2;
-            int noPick = prev1;
-            int ans = Math.max(pick,noPick);
-            prev2 = prev1;
-            prev1 = ans;
+//         int pick = nums[end] + solve(nums, end-2,start);
+//         int notpick = solve(nums, end-1, start);
+
+//         return Math.max(pick, notpick);
+//     }
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+
+//         if(n == 1) return nums[0];
+//         if(n == 2) return Math.max(nums[0], nums[1]);
+
+//         int ans1 = solve(nums, n-1, 1);
+//         int ans2 = solve(nums, n-2, 0);
+
+//         return Math.max(ans1, ans2);
+//     }
+// }
+
+
+
+// memoization
+class Solution {
+
+    public int solve(int[] nums, int end, int start, int[] dp) {
+
+        if (end < start) {
+            return 0;
         }
-        return prev1;
+
+        if (dp[end] != -1) {
+            return dp[end];
+        }
+
+        int pick = nums[end] + solve(nums, end - 2, start, dp);
+
+        int notPick = solve(nums, end - 1, start, dp);
+
+        dp[end] = Math.max(pick, notPick);
+
+        return dp[end];
     }
+
     public int rob(int[] nums) {
+
         int n = nums.length;
-        if(n == 0) return 0;
-        if(n == 1) return nums[0];
-        int[] num1 = new int[n-1];
-        int[] num2 = new int[n-1];
-        int j = 0, k = 0;
-        for(int i = 0;i<n;i++){
-            if(i != 0){
-                num1[j++] = nums[i];
-            }
-            if(i != n-1){
-                num2[k++] = nums[i];
-            }
-        }
-        return Math.max(rob1(num1),rob1(num2));
+
+        if (n == 1) return nums[0];
+        if (n == 2) return Math.max(nums[0], nums[1]);
+
+        int[] dp1 = new int[n];
+        int[] dp2 = new int[n];
+
+        Arrays.fill(dp1, -1);
+        Arrays.fill(dp2, -1);
+
+        // Case 1: Exclude first house
+        int ans1 = solve(nums, n - 1, 1, dp1);
+
+        // Case 2: Exclude last house
+        int ans2 = solve(nums, n - 2, 0, dp2);
+
+        return Math.max(ans1, ans2);
     }
 }
