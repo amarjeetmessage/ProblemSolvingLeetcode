96fb1906977471aec3f863742d6d24af35c772da
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-16 07:46:55 +0530

    Time: 0 ms (100%), Space: 41.8 MB (94.23%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -38,25 +38,50 @@
 
 
 
+// class Solution {
+//     public int solve(int n ,int[] dp) {
+
+//         if(n == 1) return 1;
+
+//         dp[1] = 1;
+//         dp[2] = 2;
+
+//         for(int i = 3; i<=n; i++){
+//             dp[i] = dp[i-1] + dp[i-2];
+//         }
+
+//         return dp[n];
+//     }
+//     public int climbStairs(int n) {
+
+//         int[] dp = new int[n+1];
+//         // Arrays.fill(dp, -1);
+    
+//        return solve(n,dp);
+//     }
+// }
+
+
+
+
 class Solution {
     public int solve(int n ,int[] dp) {
+        if(n <= 2) return n;
+        dp[0] = 1;
+        dp[1] = 2;
+        dp[2] = 3;
 
-        if(n == 1) return 1;
-
-        dp[1] = 1;
-        dp[2] = 2;
-
-        for(int i = 3; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+        for(int i = 4; i<=n; i++){
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+            dp[2] = dp[0] + dp[1];
         }
-
-        return dp[n];
+        return dp[2];
     }
     public int climbStairs(int n) {
+        
+        int[] dp = new int[3];
 
-        int[] dp = new int[n+1];
-        // Arrays.fill(dp, -1);
-    
        return solve(n,dp);
     }
 }
