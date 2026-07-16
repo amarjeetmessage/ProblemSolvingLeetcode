c2b9351eb0c3c512f9932077e29a228821107014
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-16 11:41:34 +0530

    Time: 0 ms (100%), Space: 42.7 MB (56.7%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,48 +1,49 @@
-class Solution {
-    public int rob(int[] nums) {
-        //dp always 1 based indexing
-        int n = nums.length;
-        int[] dp = new int[n+1];
-
-        dp[1] = nums[0];
-        if(n >= 2) {
-            dp[2] = Math.max(nums[0], nums[1]);
-        }
-        
-
-        for(int i = 3; i<=n; i++) {
-
-            dp[i] = Math.max(dp[i-1], nums[i-1] + dp[i-2]);
-        }
-        return dp[n];
-    }
-}
-
-
- 
-
+// // Recursoin 
+// // always give meaningfull name to function
 // class Solution {
+
+//     public int maxAmountRob(int[] nums, int n) {
+//         //base case
+//         if(n == 0) return nums[n];
+//         if(n == 1) return Math.max(nums[0], nums[1]);
+
+//         // recursive call
+//         return Math.max( nums[n] + maxAmountRob(nums,n-2), maxAmountRob(nums, n-1));
+//     }
 //     public int rob(int[] nums) {
 //         int n = nums.length;
 
-//         int[] dp = new int[n+1];
-//         Arrays.fill(dp, -1);
-
-//         return solve(nums, n-1,dp);
-//     }
-//     public static int solve(int[] nums, int n, int[] dp) {
-//         if(n == 0 ) return nums[n];
-//         if(n < 0) return 0;
-
-//         if(dp[n] != -1) {
-//             return dp[n];
-//         }
-
-//         int a = solve(nums, n-1,dp);
-//         int b = nums[n] + solve(nums, n-2,dp);
-
-//         dp[n] = Math.max(a,b);
-//         return dp[n];
+//         return maxAmountRob(nums, n-1);// n-1
 //     }
 // }
 
+
+
+
+class Solution {
+
+    public int maxAmountRob(int[] nums, int n, int[] dp) {
+        //base case
+        if(n == 0) return nums[n];
+        if(n == 1) return Math.max(nums[0], nums[1]);
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        // recursive call
+        dp[n] = Math.max( nums[n] + maxAmountRob(nums,n-2, dp), maxAmountRob(nums, n-1, dp));
+
+        return dp[n];
+    }
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+
+        return maxAmountRob(nums, n-1, dp);// n-1
+    }
+}
