16145331019126e9c6bce76737e3f97d92abfc04
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-16 07:37:45 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.55%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -13,22 +13,49 @@
 
 
 
+// class Solution {
+//     public int solve(int n, int dp[] ) {
+//          if(n == 2 || n == 1) return n;
+
+//          if(dp[n] != -1){
+//             return dp[n];
+//          }
+
+//         dp[n] =  solve(n-1,dp) + solve(n-2,dp);
+//         return dp[n];
+//     }
+//     public int climbStairs(int n) {
+
+//         int[] dp = new int[n+1];
+//         Arrays.fill(dp, -1);
+
+    
+//        return solve(n,dp);
+//     }
+// }
+
+
+
+
+
 class Solution {
-    public int solve(int n, int dp[] ) {
-         if(n == 2 || n == 1) return n;
+    public int solve(int n ,int[] dp) {
 
-         if(dp[n] != -1){
-            return dp[n];
-         }
+        if(n == 1) return 1;
 
-        dp[n] =  solve(n-1,dp) + solve(n-2,dp);
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
         return dp[n];
     }
     public int climbStairs(int n) {
 
         int[] dp = new int[n+1];
         Arrays.fill(dp, -1);
-
     
        return solve(n,dp);
     }
