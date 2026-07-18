470d6cff763710515c2aea9eb11c7bb563e7f2a1
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-18 08:40:24 +0530

    Time: 17 ms (29.56%), Space: 56.9 MB (35.39%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,21 +1,22 @@
 class Solution {
+    public int totalWays(int n, int amount, int[] coins, int[][] dp) {
+        if( n == 0) return 0;
+        if(amount < 0) return 0;
+        if( amount == 0) return 1;
+
+        if(dp[n][amount] != -1){
+            return dp[n][amount];
+        }
+
+        return dp[n][amount] = totalWays(n-1, amount, coins,dp) + totalWays(n, amount - coins[n-1], coins,dp);
+    }
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int[][] dp = new int[n + 1][amount + 1];
-
-        // Base case: 1 way to make amount 0 (empty set)
-        for (int i = 0; i <= n; i++) dp[i][0] = 1;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= amount; j++) {
-                if (coins[i - 1] <= j) {
-                    // Inclusion: use current coin (dp[i]) + Exclusion: skip current coin (dp[i-1])
-                    dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
+        int[][] dp = new int[n+1][amount+1];
+        for(int i = 0; i<n+1; i++){
+            Arrays.fill(dp[i] , -1);
         }
-        return dp[n][amount];
+
+        return totalWays(n, amount, coins,dp);
     }
 }
