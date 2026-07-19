b993f7b54daf5cc25df1b22c796f14b86898a50a
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-19 17:54:49 +0530

    Time: 0 ms (100%), Space: 43.3 MB (33.06%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -12,18 +12,54 @@
  *         this.right = right;
  *     }
  * }
- */
-class Solution {
-    public void inorder(List<Integer> list, TreeNode root){
-        if(root == null) return ;
+//  */
 
-        inorder(list, root.left);
-        list.add(root.val);
-        inorder(list, root.right);
-    }
+
+
+
+
+
+// // recursive method
+// class Solution {
+//     public void inorder(List<Integer> list, TreeNode root){
+//         if(root == null) return ;
+
+//         inorder(list, root.left);
+//         list.add(root.val);
+//         inorder(list, root.right);
+//     }
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> list = new ArrayList<>();
+//         inorder(list, root);
+//         return list;
+//     }
+// }
+
+
+
+// iterative approach
+class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-        inorder(list, root);
+
+        Stack<TreeNode> st = new Stack<>();
+
+        TreeNode node = root;
+
+        while(!st.isEmpty() || node != null) {
+           //move to left
+           while(node != null){
+            st.push(node);
+            node = node.left;
+           }
+
+           //print root
+            node = st.pop();
+            list.add(node.val);
+
+           //move to right subtree
+           node = node.right;
+        }
         return list;
     }
 }
