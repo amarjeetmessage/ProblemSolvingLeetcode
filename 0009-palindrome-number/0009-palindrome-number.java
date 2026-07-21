183993dafda3a775955bb77d6ed8a8c25a7f6b5e
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-21 16:50:59 +0530

    Time: 6 ms (21.87%), Space: 46.2 MB (6.45%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,16 +1,39 @@
+// class Solution {
+//     public boolean isPalindrome(int x) {
+//         //convert number to string
+//         String str = String.valueOf(x);
+//         //apply two pointer check from both side for length/2
+//         int i = 0;
+//         int j = str.length() -1 ;
+//         while(i<j) {
+//             if(str.charAt(i) != str.charAt(j)) {
+//                 return false;
+//             }
+//             i++;
+//             j--;
+//         }
+//         return true;
+//     }
+// }
+
+
+
+
 class Solution {
     public boolean isPalindrome(int x) {
-        //convert number to string
-        String str = String.valueOf(x);
-        //apply two pointer check from both side for length/2
-        int i = 0;
-        int j = str.length() -1 ;
-        while(i<j) {
-            if(str.charAt(i) != str.charAt(j)) {
+        String s = String.valueOf(x);
+        int n = s.length();
+        
+        int start = 0;
+        int end = n-1;
+
+        while(start < end) {
+            if(s.charAt(start) == s.charAt(end)){
+                start++;
+                end--;
+            }else{
                 return false;
             }
-            i++;
-            j--;
         }
         return true;
     }
