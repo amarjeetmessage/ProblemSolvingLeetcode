ec10578c1a754e3ebbd8b25a3ee81dbca3a060f1
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-22 15:47:16 +0530

    Time: 1 ms (31.73%), Space: 45.6 MB (41.73%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -13,25 +13,62 @@
  *     }
  * }
  */
+
+// class Solution {
+//     public int solve(TreeNode root){
+//         if(root == null) return 0;
+//         int left = solve(root.left);
+//         int right = solve(root.right);
+
+//         int max = Math.max(left, right) + 1;
+//         return max;
+
+//     }
+//     public boolean isBalanced(TreeNode root) {
+//         if(root == null) return true;
+//         int m1 = solve(root.left);
+//         int m2 = solve(root.right);
+
+//         if(Math.abs(m1 - m2) > 1){
+//             return false;
+//         }
+
+//         return isBalanced(root.left) && isBalanced(root.right); // this will check further left and right
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
 class Solution {
-    public int solve(TreeNode root){
+
+    public int getHeight(TreeNode root) {
         if(root == null) return 0;
-        int left = solve(root.left);
-        int right = solve(root.right);
 
-        int max = Math.max(left, right) + 1;
-        return max;
+        int leftSubTreeHeight = getHeight(root.left);
+        int rightSubTreeHeight = getHeight(root.right);
 
-    }
-    public boolean isBalanced(TreeNode root) {
-        if(root == null) return true;
-        int m1 = solve(root.left);
-        int m2 = solve(root.right);
-
-        if(Math.abs(m1 - m2) > 1){
-            return false;
+        // If any subtree is already unbalanced
+        if(leftSubTreeHeight == -1 || rightSubTreeHeight == -1) {
+            return -1;
         }
 
-        return isBalanced(root.left) && isBalanced(root.right);
+        // Check current node balance
+        if(Math.abs(leftSubTreeHeight - rightSubTreeHeight) > 1) {
+            return -1;
+        }
+
+        // Return height of current subtree
+        return Math.max(leftSubTreeHeight, rightSubTreeHeight) + 1;
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        return getHeight(root) != -1;
     }
 }
