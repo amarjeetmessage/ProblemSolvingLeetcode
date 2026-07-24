e3901e758f4e74a79922ec2ac8cb051b807c02d8
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-24 19:11:23 +0530

    Time: 1 ms (28.3%), Space: 42.1 MB (98.18%) - LeetHub

--- 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
@@ -1,14 +1,33 @@
+
+// class Solution {
+//     public int countSegments(String s) {
+//         int n = s.length();
+//         int count = 0;
+
+//         for(int i = 0; i<n; i++) {
+//             if(s.charAt(i) != ' ' && (i == 0 ||s.charAt(i-1)== ' ')){
+//                 count++;
+//             }
+//         }
+
+//         return count;
+//     }
+// }
+
+
+
+
 class Solution {
     public int countSegments(String s) {
-        int n = s.length();
-        int count = 0;
 
-        for(int i = 0; i<n; i++) {
-            if(s.charAt(i) != ' ' && (i == 0 ||s.charAt(i-1)== ' ')){
-                count++;
-            }
+        s = s.trim();
+
+        if (s.isEmpty()) {
+            return 0;
         }
 
-        return count;
+        String[] ans = s.split("\\s+");
+
+        return ans.length;
     }
 }
