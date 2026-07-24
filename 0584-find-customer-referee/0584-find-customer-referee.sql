656a9e91975f5a4dd99611e35f6e45ec92dfec92
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-07-24 17:22:40 +0530

    Time: 554 ms (21.73%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,12 +1,5 @@
 # Write your MySQL query statement below
-SELECT name FROM Customer
-WHERE referee_id <> 2 OR  referee_id IS NULL;
--- WHERE referee_id != 2 OR  referee_id IS NULL;
-
-
-
--- We want to return customers who are:
-
--- Referred by someone whose id != 2 ✅
-
--- NOT referred by anyone (means referee_id IS NULL) ✅
+select name 
+from Customer
+where referee_id != 2 OR referee_id is null              
+# we can not check like referee_id = null
