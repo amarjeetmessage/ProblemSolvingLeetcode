253738c11a0eb5f2f5f8d93a641ef54ea98ceb5a
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-08-01 23:02:41 +0530

    Time: 67 ms (19.36%), Space: 42.7 MB (74.96%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solve(int[] nums, int i , int j) {
+        if(i == j) return nums[i];
+
+        // recursive case
+        int left = nums[i] - solve(nums, i+1, j);
+        int right = nums[j] - solve(nums, i, j-1);
+
+        return Math.max(left, right);
+    }
+    public boolean predictTheWinner(int[] nums) {
+        return solve(nums, 0, nums.length -1) >= 0;
+    }
+}
