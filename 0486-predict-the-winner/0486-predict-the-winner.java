5b2b53e5bcc7a2964064531626284e27a598afd0
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-08-01 23:08:34 +0530

    Time: 0 ms (100%), Space: 42.8 MB (57.54%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,14 +1,42 @@
+// class Solution {
+//     public int solve(int[] nums, int i , int j) {
+//         if(i == j) return nums[i];
+
+//         // recursive case
+//         int left = nums[i] - solve(nums, i+1, j);
+//         int right = nums[j] - solve(nums, i, j-1);
+
+//         return Math.max(left, right);
+//     }
+//     public boolean predictTheWinner(int[] nums) {
+//         return solve(nums, 0, nums.length -1) >= 0;
+//     }
+// }
+
+
+
+
+
 class Solution {
-    public int solve(int[] nums, int i , int j) {
+    public int solve(int[] nums, int i , int j, int[][] dp) {
         if(i == j) return nums[i];
 
-        // recursive case
-        int left = nums[i] - solve(nums, i+1, j);
-        int right = nums[j] - solve(nums, i, j-1);
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
 
-        return Math.max(left, right);
+        // recursive case
+        int left = nums[i] - solve(nums, i+1, j,dp);
+        int right = nums[j] - solve(nums, i, j-1, dp);
+
+        return dp[i][j] = Math.max(left, right);
     }
     public boolean predictTheWinner(int[] nums) {
-        return solve(nums, 0, nums.length -1) >= 0;
+        int n = nums.length;
+        int[][] dp = new int[n+1][n+1];
+        for(int i = 0; i<=n; i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return solve(nums, 0, n-1,dp) >= 0;
     }
 }
