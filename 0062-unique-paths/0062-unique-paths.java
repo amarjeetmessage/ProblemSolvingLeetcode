79ee6d43ec118efd35a88ae2fee9e24f1a121226
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-08-08 19:30:48 +0530

    Time: 0 ms (100%), Space: 42.3 MB (29.54%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,56 +1,23 @@
-// class Solution {
-//     public int uniquePaths(int m, int n) {
-    
-//     int[][] dp = new int[m+1][n+1];
-
-//     for(int[] row : dp) {
-//         Arrays.fill(row , -1);
-//     }
-
-//     return solve(0, 0, m, n,dp);
-
-//     }
-
-//     public int solve(int i, int j , int m, int n,int[][] dp) {
-//         if(i == m-1 && j == n -1) return 1;
-//         if(i >= m || j>=n) return 0;
-
-//         if(dp[i][j] != -1) {
-//             return dp[i][j];
-//         }
-
-//         int a = solve(i, j+1,m,n,dp);
-//         int b = solve(i+1, j,m,n,dp);
-
-//         dp[i][j] = a+b;
-//         return dp[i][j];
-//     }
-// }
 class Solution {
     public int uniquePaths(int m, int n) {
+    int[][] dp = new int[m+1][n+1];
+    for(int i = 0; i<=m; i++){
+        Arrays.fill(dp[i], -1);
+    }
+    return solve(0,0,m,n,dp);
 
-        int[][] dp = new int[m][n];
+    }
+    public int solve(int i, int j, int m, int n,int[][] dp) {
+        // base case 
+        if(i == m-1 && j == n-1) return 1;
+        if(i >=m || j >= n) return 0;
 
-        // base case
-        dp[0][0] = 1;
-
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-
-                if(i == 0 && j == 0) continue;
-
-                int up = 0;
-                int left = 0;
-
-                if(i > 0) up = dp[i-1][j];
-                if(j > 0) left = dp[i][j-1];
-
-                dp[i][j] = up + left;
-            }
+        if(dp[i][j] != -1){
+            return dp[i][j];
         }
 
-        return dp[m-1][n-1];
+        int a = solve(i, j+1, m,n,dp);
+        int b = solve(i+1, j, m, n,dp) ;
+        return dp[i][j] = a + b;
     }
 }
-
-//fill last row and column by 1 as base case for tabulation
