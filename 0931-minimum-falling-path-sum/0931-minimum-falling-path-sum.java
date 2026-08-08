df653c0385cd39839752ec79a3fba0abcc1b7106
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-08-08 08:00:43 +0530

    Time: 1 ms (99.63%), Space: 47.4 MB (17.77%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -3,40 +3,36 @@ class Solution {
 
         int n = matrix.length;
 
+        int[][] dp = new int[n][n];
+        for(int i = 0; i<n; i++) {
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        }
+
         int mn = Integer.MAX_VALUE;
         for(int j = 0; j<n; j++) {
-            int ans = solve(matrix, n, 0 ,j);
+            int ans = solve(matrix, n, 0 ,j,dp );
             mn = Math.min(mn, ans);
         }
         return mn;
     }
-    public static int solve(int[][] matrix, int n, int i , int j) {
+    public static int solve(int[][] matrix, int n, int i , int j,int[][] dp) {
         if(j < 0 || j > n-1) return Integer.MAX_VALUE;
         if(i == n-1) return matrix[i][j];
 
-        //RECURSIVE CALL
-        int a = solve(matrix, n,i+1,j-1);
-        int b = solve(matrix, n,i+1,j+1);
-        int c = solve(matrix, n,i+1,j);
+        if(dp[i][j] != Integer.MAX_VALUE) {
+            return dp[i][j];
+        }
 
-       return Math.min(c,Math.min(a,b)) + matrix[i][j];
-       
+        //RECURSIVE CALL
+        int a = solve(matrix, n,i+1,j-1,dp);
+        int b = solve(matrix, n,i+1,j+1,dp);
+        int c = solve(matrix, n,i+1,j,dp);
+
+        dp[i][j] =  Math.min(c,Math.min(a,b)) + matrix[i][j];
+        return dp[i][j];
     
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // class Solution {
