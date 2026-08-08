6a7001d5601cd48361bf2d7a2e784a8c920b4392
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-08-08 19:43:52 +0530

    Time: 0 ms (100%), Space: 43.4 MB (81.76%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -82,41 +82,104 @@
 
 
 
+// class Solution {
+//     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+
+//         int m = obstacleGrid.length;
+//         int n = obstacleGrid[0].length;
+
+//         int[][] dp = new int[m + 1][n + 1];
+
+//         if (obstacleGrid[m - 1][n - 1] == 1) {
+//             return 0;//b/c not possible to reach at destination
+//         }
+
+//         // for(int i=0;i<=m;i++){
+//         //     dp[i][n]=0;
+//         // }
+//         // for(int j=0;j<=n;j++){
+//         //     dp[m][j]=0;
+//         // }
+
+//         for (int i = m - 1; i >= 0; i--) {
+//             for (int j = n - 1; j >= 0; j--) {
+
+//                 if (i == m - 1 && j == n - 1) {
+//                     dp[i][j] = 1;
+//                     continue;
+//                 }
+
+//                 if (obstacleGrid[i][j] == 1) {
+//                     dp[i][j] = 0;
+//                 } else {
+//                     dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+//                 }
+//             }
+//         }
+
+//         return dp[0][0];
+//     }
+// }
+
+
+
+
+// class Solution {
+//     public int uniquePathsWithObstacles(int[][] grid) {
+//         int m = grid.length;
+//         int n = grid[0].length;
+//         return solve(0,0,m,n,grid);
+//     }
+//     public int solve(int i, int j, int m, int n, int[][] grid) {
+//         // write boundary condition before base case
+//         if(i >= m || j >= n) return 0;
+
+//         // write this one extra than unique path 1 is we will return 0 if obstackle occurs
+//         if(grid[i][j] == 1) return 0;
+
+//         // base case
+//         if(i == m-1 && j == n-1) return 1;
+//         // recursion
+//         int right = solve(i, j+1,m,n,grid);
+//         int down = solve(i+1, j, m, n, grid);
+//         return right + down;
+//     }
+// }
+
+
+
+
+
+
+
+
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
 
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-
-        int[][] dp = new int[m + 1][n + 1];
-
-        if (obstacleGrid[m - 1][n - 1] == 1) {
-            return 0;//b/c not possible to reach at destination
+        int[][] dp = new int[m+1][n+1];
+        for(int i = 0; i<=m;i++){
+            Arrays.fill(dp[i], -1);
         }
+        return solve(0,0,m,n,grid,dp);
+    }
+    public int solve(int i, int j, int m, int n, int[][] grid, int[][] dp) {
+        // write boundary condition before base case
+        if(i >= m || j >= n) return 0;
 
-        // for(int i=0;i<=m;i++){
-        //     dp[i][n]=0;
-        // }
-        // for(int j=0;j<=n;j++){
-        //     dp[m][j]=0;
-        // }
+        // write this one extra than unique path 1 is we will return 0 if obstackle occurs
+        if(grid[i][j] == 1) return 0;
 
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
+        // base case
+        if(i == m-1 && j == n-1) return 1;
 
-                if (i == m - 1 && j == n - 1) {
-                    dp[i][j] = 1;
-                    continue;
-                }
-
-                if (obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0;
-                } else {
-                    dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
-                }
-            }
+        if(dp[i][j] != -1){
+            return dp[i][j];
         }
-
-        return dp[0][0];
+        // recursion
+        int right = solve(i, j+1,m,n,grid,dp);
+        int down = solve(i+1, j, m, n, grid,dp);
+        return dp[i][j] = right + down;
     }
 }
