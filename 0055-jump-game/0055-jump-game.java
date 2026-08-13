6ff956fa90a55c66b6595e7218b65e24a4d2d5f1
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-08-13 23:46:42 +0530

    Time: 3 ms (20.77%), Space: 48.1 MB (23.89%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,17 +1,34 @@
 class Solution {
     public boolean canJump(int[] nums) {
-
-        int maxi = 0;
         int n = nums.length;
-
-        for(int i = 0; i < nums.length; i++){
-            maxi = Math.max(maxi, i + nums[i]);
-
-            if(maxi == n - 1) break;
-
-            if(i == maxi) return false;
+        if(n == 1) return true;
+        int idx = 0;
+        for(int i = 0; i<n-1; i++){
+            if(i > idx) return false;
+            idx = Math.max(idx, i + nums[i]);
         }
 
-        return true;
+        return idx>=n-1;
     }
 }
+
+
+
+
+// class Solution {
+//     public boolean canJump(int[] nums) {
+
+//         int maxi = 0;
+//         int n = nums.length;
+
+//         for(int i = 0; i < nums.length; i++){
+//             maxi = Math.max(maxi, i + nums[i]);
+
+//             if(maxi == n - 1) break;
+
+//             if(i == maxi) return false;
+//         }
+
+//         return true;
+//     }
+// }
