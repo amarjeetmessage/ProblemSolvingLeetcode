d72c2d189e17aa6331c27f57b629a52773825852
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-08-23 01:39:14 +0530

    Time: 32 ms (16.81%), Space: 124.7 MB (5.13%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,65 +1,33 @@
-// class Solution {
-//     public boolean containsNearbyDuplicate(int[] nums, int k) {
-//       for(int i = 0; i<nums.length-1; i++) {
-//         for(int j = i+1; j<nums.length; j++) {
-//             if(nums[i] == nums[j]) {
-//                 if(Math.abs(i-j) <= k){
-//                     return true;
-//                 }
-//             }else {
-//                 continue;
-//             }
-//         }
-//       } 
-//       return false; 
-//     }
-// }
-
-
-
-
-
-// class Solution {
-//     public boolean containsNearbyDuplicate(int[] nums, int k) {
-//       for(int i = 0; i<nums.length; i++) {
-//         Set<Integer> set = new HashSet<>();
-//         for(int j = i; j<= Math.min(i+k,nums.length-1) ; j++) {
-//             if(set.contains(nums[j])) {
-//                 return true;
-//             }else{ 
-//                 set.add(nums[j]);
-//             }
-//         }
-//       }
-//         return false;
-//     }
-// }
-
-
-
-
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Set<Integer> set = new HashSet<>();
-        for(int i = 0; i < Math.min(k , nums.length); i++) {
-            if(set.contains(nums[i])) {
-                return true;
-            }else{
-                set.add(nums[i]);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i< nums.length; i++){
+            if(map.containsKey(nums[i])){
+                int val = map.get(nums[i]);
+                if((i - val) <= k) return true;
             }
-        }
-
-        for(int i = k ; i < nums.length; i++) {
-            if(set.contains(nums[i])) {
-                return true;
-            }else{
-                set.add(nums[i]);
-                set.remove(nums[i-k]);
-            }
+            map.put(nums[i], i);
         }
         return false;
     }
 }
 
+// 1 2 3 1 
+// i
+// map: 
 
-// Math.min(k , nums.length) THIS IS NEEDED BECAUSE IF K > NUMS.LENGTH CASE
+
+// class Solution {
+//     public boolean containsNearbyDuplicate(int[] nums, int k) {
+//         for(int i = 0; i<nums.length; i++){
+//             for(int j = i+1; j<nums.length; j++){
+//                 if(nums[i] == nums[j]) {
+//                     if(Math.abs(i-j) <=k){
+//                         return true;
+//                     }
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// }
