ccac79b199b4e47165ed23b519da665d3d8c097f
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-08-23 12:46:47 +0530

    Time: 10 ms (16.06%), Space: 47.1 MB (26.28%) - LeetHub

--- 1927-sum-game/1927-sum-game.java
+++ 1927-sum-game/1927-sum-game.java
@@ -1,70 +1,26 @@
-// class Solution {
-//     public boolean sumGame(String num) {
-
-//         int n = num.length();
-
-//         int sum1 = 0, sum2 = 0;
-//         int count1 = 0, count2 = 0;
-
-//         for (int i = 0; i < n / 2; i++) {
-//             if (num.charAt(i) == '?') {
-//                 count1++;
-//             } else {
-//                 sum1 += num.charAt(i) - '0';
-//             }
-//         }
-
-//         for (int i = n / 2; i < n; i++) {
-//             if (num.charAt(i) == '?') {
-//                 count2++;
-//             } else {
-//                 sum2 += num.charAt(i) - '0';
-//             }
-//         }
-
-//         int diff = sum1 - sum2;
-//         int countDiff = count1 - count2;
-
-//         // Alice wins if she can force an unequal sum
-//         if ((countDiff % 2 != 0)) {
-//             return true;
-//         }
-
-//         return diff != (countDiff / 2) * 9;
-//     }
-// }
-
-
-
-
-
-
-
-
-
 class Solution {
     public boolean sumGame(String num) {
 
         int n = num.length();
 
         int sum1 = 0, sum2 = 0;
-        int q1 = 0, q2 = 0;
+        int count1 = 0, count2 = 0;
 
         for (int i = 0; i < n / 2; i++) {
             if (num.charAt(i) == '?')
-                q1++;
+                count1++;
             else
                 sum1 += num.charAt(i) - '0';
         }
 
         for (int i = n / 2; i < n; i++) {
             if (num.charAt(i) == '?')
-                q2++;
+                count2++;
             else
                 sum2 += num.charAt(i) - '0';
         }
 
-        int qDiff = q1 - q2;
+        int qDiff = count1 - count2;
         int sumDiff = sum1 - sum2;
 
         if (qDiff % 2 != 0) {
